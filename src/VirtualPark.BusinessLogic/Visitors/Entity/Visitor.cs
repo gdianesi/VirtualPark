b1a88f4cd30617f02b2aae684484e078bcd76ae7
gdianesi <gd329180@fi365.ort.edu.uy>
2025-09-20 18:39:06 -0300

    REFACTOR: name can not be null or empty

--- src/VirtualPark.BusinessLogic/Visitors/Entity/Visitor.cs
+++ src/VirtualPark.BusinessLogic/Visitors/Entity/Visitor.cs
@@ -2,42 +2,44 @@ namespace VirtualPark.BusinessLogic.Visitors.Entity;
 
 public sealed class Visitor
 {
-    public Guid Id { get; init; } = Guid.NewGuid();
+    public Guid Id { get; } = Guid.NewGuid();
 
     private DateTime _dateOfBirth;
-
     public DateTime DateOfBirth
     {
         get => _dateOfBirth;
-        set
-        {
-            if(value > DateTime.UtcNow)
-            {
-                throw new ArgumentException("Date of birth cannot be in the future");
-            }
-
-            _dateOfBirth = value;
-        }
+        set => _dateOfBirth = ValidateDateOfBirth(value);
     }
 
     private string _name = string.Empty;
-
     public string Name
     {
         get => _name;
-        set
-        {
-            if (string.IsNullOrWhiteSpace(value))
-            {
-                throw new ArgumentException("Name cannot be null or empty");
-            }
-
-            _name = value;
-        }
+        set => _name = ValidateName(value);
     }
 
     public Visitor(string name)
     {
         Name = name;
+    }
+
+    private static DateTime ValidateDateOfBirth(DateTime date)
+    {
+        if (date > DateTime.UtcNow)
+        {
+            throw new ArgumentException("Date of birth cannot be in the future", nameof(date));
+        }
+
+        return date;
+    }
+
+    private static string ValidateName(string name)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+        {
+            throw new ArgumentException("Name cannot be null or empty", nameof(name));
+        }
+
+        return name;
     }
 }
