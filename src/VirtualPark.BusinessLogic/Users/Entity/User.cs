71609d8eb427817edf717afafa973cf82ed63b5c
Alfonso Ricaldoni <AR287195@fi365.ort.edu.uy>
2025-09-20 20:19:09 -0300

    GREEN: the name must be gettable

--- src/VirtualPark.BusinessLogic/Users/Entity/User.cs
+++ src/VirtualPark.BusinessLogic/Users/Entity/User.cs
@@ -3,6 +3,7 @@ namespace VirtualPark.BusinessLogic.Users.Entity;
 public sealed class User
 {
     public Guid Id { get; }
+    public string Name { get; init; } = null!;
 
     public User()
     {
