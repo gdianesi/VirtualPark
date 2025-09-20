2b37f34ad4c63bbdaedcc92c1ab9bb8e654083da
Alfonso Ricaldoni <AR287195@fi365.ort.edu.uy>
2025-09-20 20:56:22 -0300

    GREEN: the password must be settable

--- src/VirtualPark.BusinessLogic/Users/Entity/User.cs
+++ src/VirtualPark.BusinessLogic/Users/Entity/User.cs
@@ -6,7 +6,7 @@ public sealed class User
     public string Name { get; set; } = null!;
     public string LastName { get; set; } = null!;
     public string Email { get; set; } = null!;
-    public string Password { get; init; } = null!;
+    public string Password { get; set; } = null!;
 
     public User()
     {
