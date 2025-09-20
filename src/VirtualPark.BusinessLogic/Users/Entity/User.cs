c50d07e3b540f97a3f7a2a163524f4146b2124a9
Alfonso Ricaldoni <AR287195@fi365.ort.edu.uy>
2025-09-20 20:27:18 -0300

    GREEN: the last name must be gettable

--- src/VirtualPark.BusinessLogic/Users/Entity/User.cs
+++ src/VirtualPark.BusinessLogic/Users/Entity/User.cs
@@ -5,6 +5,8 @@ public sealed class User
     public Guid Id { get; }
     public string Name { get; set; } = null!;
 
+    public string LastName { get; init; } = null!;
+
     public User()
     {
         Id = Guid.NewGuid();
