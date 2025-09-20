8a3b032a41bd089577b4bd0a4748e39263616655
Alfonso Ricaldoni <AR287195@fi365.ort.edu.uy>
2025-09-20 20:44:40 -0300

    GREEN: the email  must be gettable

--- src/VirtualPark.BusinessLogic/Users/Entity/User.cs
+++ src/VirtualPark.BusinessLogic/Users/Entity/User.cs
@@ -4,8 +4,8 @@ public sealed class User
 {
     public Guid Id { get; }
     public string Name { get; set; } = null!;
-
     public string LastName { get; set; } = null!;
+    public string Email { get; init; } = null!;
 
     public User()
     {
