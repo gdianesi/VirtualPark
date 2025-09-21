47a2f3523b6e537228a1e205b03ae43247f7300a
gdianesi <gd329180@fi365.ort.edu.uy>
2025-09-21 18:00:47 -0300

    GREEN: Key should return assigned key

--- src/VirtualPark.BusinessLogic/Permissions/Entity/Permission.cs
+++ src/VirtualPark.BusinessLogic/Permissions/Entity/Permission.cs
@@ -4,6 +4,7 @@ public class Permission
 {
     public Guid Id { get; }
     public string Description { get; set; } = null!;
+    public string Key { get; set; } = null!;
 
     public Permission()
     {
