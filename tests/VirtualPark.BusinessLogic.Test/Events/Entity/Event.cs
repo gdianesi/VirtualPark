4177e7e1f723fd981d850ffaea10788478236afa
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-09-21 20:07:14 -0300

    RED:  WhenEventIsCreated_IdIsAssigned

--- tests/VirtualPark.BusinessLogic.Test/Events/Entity/Event.cs
+++ tests/VirtualPark.BusinessLogic.Test/Events/Entity/Event.cs
@@ -1,9 +1,21 @@
+using FluentAssertions;
+using VirtualPark.BusinessLogic.Attractions.Entity;
+using VirtualPark.BusinessLogic.Events;
+
 namespace VirtualPark.BusinessLogic.Test.Events.Entity;
 
 [TestClass]
 [TestCategory("Entity")]
 [TestCategory("Events")]
-public class Event
+public sealed class Event
 {
+    #region ID
+    [TestMethod]
+    public void WhenEventIsCreated_IdIsAssigned()
+    {
+        var newEvent = new Event();
+        newEvent.Id.Should().NotBe(Guid.Empty);
+    }
+    #endregion
 
 }
