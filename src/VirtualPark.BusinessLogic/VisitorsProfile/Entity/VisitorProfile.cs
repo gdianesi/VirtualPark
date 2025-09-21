a4dac44fbc7a02925a491c367a139e74288f7c1c
Alfonso Ricaldoni <AR287195@fi365.ort.edu.uy>
2025-09-21 17:41:03 -0300

    GREEN: the DateOfBirth must be gettable

--- src/VirtualPark.BusinessLogic/VisitorsProfile/Entity/VisitorProfile.cs
+++ src/VirtualPark.BusinessLogic/VisitorsProfile/Entity/VisitorProfile.cs
@@ -3,6 +3,7 @@ namespace VirtualPark.BusinessLogic.VisitorsProfile.Entity;
 public class VisitorProfile
 {
     public Guid Id { get; init; }
+    public DateOnly DateOfBirth { get; init; }
 
     public VisitorProfile()
     {
