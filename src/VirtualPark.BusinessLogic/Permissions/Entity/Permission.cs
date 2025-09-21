826f34329f692500baae67d495d03303f2a2108b
gdianesi <gd329180@fi365.ort.edu.uy>
2025-09-21 20:41:50 -0300

    GREEN: Permission should initialize Roles list

--- src/VirtualPark.BusinessLogic/Permissions/Entity/Permission.cs
+++ src/VirtualPark.BusinessLogic/Permissions/Entity/Permission.cs
@@ -1,10 +1,11 @@
 namespace VirtualPark.BusinessLogic.Permissions.Entity;
-
+using Roles.Entity;
 public class Permission
 {
     public Guid Id { get; }
     public string Description { get; set; } = null!;
     public string Key { get; set; } = null!;
+    public List<Role> Roles { get; set; } = new();
 
     public Permission()
     {
