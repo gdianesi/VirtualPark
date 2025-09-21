7250d790ce3e8e849194dc4b3326e5e89177efef
Alfonso Ricaldoni <AR287195@fi365.ort.edu.uy>
2025-09-21 20:37:44 -0300

    GREEN: the Type must be gettable

--- src/VirtualPark.BusinessLogic/Incidences/Entity/Incidence.cs
+++ src/VirtualPark.BusinessLogic/Incidences/Entity/Incidence.cs
@@ -1,8 +1,11 @@
+using VirtualPark.BusinessLogic.TypeIncidences.Entity;
+
 namespace VirtualPark.BusinessLogic.Incidences.Entity;
 
 public class Incidence
 {
     public Guid Id { get; }
+    public TypeIncidence Type { get; init; } = null!;
 
     public Incidence()
     {
