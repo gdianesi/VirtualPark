36c50edf8885b983eceba31eaea96c3e07c5c7d8
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-09-21 22:31:42 -0300

    GREEN: WhenRankingIsCreated_ListEntriesIsAssigned

--- src/VirtualPark.BusinessLogic/Rankings/Entity/Ranking.cs
+++ src/VirtualPark.BusinessLogic/Rankings/Entity/Ranking.cs
@@ -1,7 +1,10 @@
+using VirtualPark.BusinessLogic.Users.Entity;
+
 namespace VirtualPark.BusinessLogic.Rankings.Entity;
 
 public sealed class Ranking
 {
     public Guid Id { get; init; } = Guid.NewGuid();
     public DateTime Date { get; set; }
+    public List<User> Entries { get; set; } = [];
 }
