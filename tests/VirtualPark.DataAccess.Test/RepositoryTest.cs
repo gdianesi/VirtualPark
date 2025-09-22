92a96bc59cfce5c172bd6930ae9f953a32476905
Alfonso Ricaldoni <AR287195@fi365.ort.edu.uy>
2025-09-22 18:53:58 -0300

    RED: get all repository

--- tests/VirtualPark.DataAccess.Test/RepositoryTest.cs
+++ tests/VirtualPark.DataAccess.Test/RepositoryTest.cs
@@ -8,6 +8,42 @@ namespace VirtualPark.DataAccess.Test;
 [TestCategory("Repository")]
 public class RepositoryTest
 {
+    private readonly DbContext _context = SqliteInMemoryDbContext.BuildTestDbContext();
+    private readonly Repository _repository;
+
+    public RepositoryTest()
+    {
+        _repository = new Repository<EntityTest>(_context);
+    }
+
+    [TestInitialize]
+    public void Setup()
+    {
+        _context.Database.EnsureCreated();
+    }
+
+    [TestCleanup]
+    public void Cleanup()
+    {
+        _context.Database.EnsureDeleted();
+    }
+
+    [TestMethod]
+    public void GetAll_Ok()
+    {
+        var e1 = new EntityTest { Id = Guid.NewGuid().ToString() };
+        var e2 = new EntityTest { Id = Guid.NewGuid().ToString() };
+
+        _context.Set<EntityTest>().AddRange(e1, e2);
+        _context.SaveChanges();
+
+        var result = _repository.GetAll();
+
+        result.Should().NotBeNull();
+        result.Should().HaveCount(2);
+        result.Should().ContainEquivalentOf(e1);
+        result.Should().ContainEquivalentOf(e2);
+    }
 }
 
 internal sealed class TestDbContext : DbContext
