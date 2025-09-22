fc30afb216ed2c64137c1c604b5369f833adc0bc
gdianesi <gd329180@fi365.ort.edu.uy>
2025-09-21 21:36:22 -0300

    GREEN: VisitRegistration should initialize Date

--- src/VirtualPark.BusinessLogic/VisitRegistrations/Entity/VisitRegistration.cs
+++ src/VirtualPark.BusinessLogic/VisitRegistrations/Entity/VisitRegistration.cs
@@ -3,4 +3,5 @@ namespace VirtualPark.BusinessLogic.VisitRegistrations.Entity;
 public sealed class VisitRegistration
 {
     public Guid Id { get; } = Guid.NewGuid();
+    public DateTime Date { get; set; } = DateTime.Today;
 }
