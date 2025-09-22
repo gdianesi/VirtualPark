654bd354e3e0a0cdd4c1ba4d56f37ab7f412a70e
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-09-21 22:10:26 -0300

    RED: WhenRankingIsCreated_IdIsAssigned

---
+++ tests/VirtualPark.BusinessLogic.Test/Rankings/Entity/RankingTest.cs
@@ -0,0 +1,16 @@
+namespace VirtualPark.BusinessLogic.Test.Rankings.Entity;
+
+[TestClass]
+[TestCategory("Entity")]
+[TestCategory("Ranking")]
+public class RankingTest
+{
+    #region id
+    [TestMethod]
+    public void WhenRankingIsCreated_IdIsAssigned()
+    {
+        var ranking = new Ranking();
+        ranking.Id.Should().NotBe(Guid.Empty);
+    }
+    #endregion
+}
