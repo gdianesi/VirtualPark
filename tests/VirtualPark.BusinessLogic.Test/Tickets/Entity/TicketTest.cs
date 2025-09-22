d28d6de673bc829b14d5fc01ce51f248aadf9ca7
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-09-21 21:32:38 -0300

    RED: Date_GetSet_Works

---
+++ tests/VirtualPark.BusinessLogic.Test/Tickets/Entity/TicketTest.cs
@@ -0,0 +1,27 @@
+using FluentAssertions;
+using VirtualPark.BusinessLogic.Tickets.Entity;
+
+namespace VirtualPark.BusinessLogic.Test.Tickets.Entity;
+
+[TestClass]
+[TestCategory("Entity")]
+[TestCategory("Ticket")]
+public sealed class TicketTest
+{
+    #region ID
+    [TestMethod]
+    public void WhenTicketIsCreated_IdIsAssigned()
+    {
+        var ticket = new Ticket();
+        ticket.Id.Should().NotBe(Guid.Empty);
+    }
+    #endregion
+    #region Date
+
+    public void Date_GetSet_Works()
+    {
+        var ticket = new Ticket{ Date = DateTime.Now };
+        ticket.Date.Should().BeCloseTo(ticket.Date, precision: TimeSpan.FromMilliseconds(10));
+    }
+    #endregion
+}
