67a3d27d095aba808e5703adf8d0661a8fd8d84e
gdianesi <gd329180@fi365.ort.edu.uy>
2025-09-22 19:26:37 -0300

    REFACTOR: Visitor entity refactor

--- src/VirtualPark.BusinessLogic/Visitors/Entity/Visitor.cs
+++ src/VirtualPark.BusinessLogic/Visitors/Entity/Visitor.cs
@@ -2,12 +2,12 @@ namespace VirtualPark.BusinessLogic.Visitors.Entity;
 
 public class Visitor
 {
-    public Guid Id { get; init; } =  Guid.NewGuid();
+    public Guid Id { get; } = Guid.NewGuid();
     public string Name { get; set; } = null!;
-    public object LastName { get; set; } = null!;
-    public object Email { get; set; } = null!;
-    public object PasswordHash { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string PasswordHash { get; set; } = null!;
     public DateTime DateOfBirth { get; set; }
-    public object Score { get; set; } = 0;
-    public Membership Membership { get; set; } = Membership.Standard;
+    public int Score { get; private set; } = 0;
+    public Membership Membership { get; private set; } = Membership.Standard;
 }
