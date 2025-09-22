568c56911639811e14d33f8228d22258e9856bad
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-09-21 22:11:45 -0300

    GREEN: WhenRankingIsCreated_IdIsAssigned

--- src/VirtualPark.BusinessLogic/Rankings/Entity/Ranking.cs
+++ src/VirtualPark.BusinessLogic/Rankings/Entity/Ranking.cs
@@ -2,4 +2,5 @@ namespace VirtualPark.BusinessLogic.Rankings.Entity;
 
 public sealed class Ranking
 {
+    public Guid Id { get; set; } = Guid.NewGuid();
 }
--- tests/VirtualPark.BusinessLogic.Test/Rankings/Entity/RankingTest.cs
+++ tests/VirtualPark.BusinessLogic.Test/Rankings/Entity/RankingTest.cs
@@ -1,3 +1,6 @@
+using FluentAssertions;
+using VirtualPark.BusinessLogic.Rankings.Entity;
+
 namespace VirtualPark.BusinessLogic.Test.Rankings.Entity;
 
 [TestClass]
