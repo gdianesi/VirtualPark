cb4457ac6121cd467ab4d7452f755a21d42d669c
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-09-22 22:34:07 -0300

    REFACTOR: id incidence

--- src/VirtualPark.BusinessLogic/Incidences/Entity/Incidence.cs
+++ src/VirtualPark.BusinessLogic/Incidences/Entity/Incidence.cs
@@ -4,12 +4,7 @@ namespace VirtualPark.BusinessLogic.Incidences.Entity;
 
 public class Incidence
 {
-    public Guid Id { get; }
+    public Guid Id { get; init; } = Guid.NewGuid();
     public TypeIncidence Type { get; set; } = null!;
     public string Description { get; init; } = null!;
-
-    public Incidence()
-    {
-        Id = Guid.NewGuid();
-    }
 }
