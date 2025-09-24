46236f4074627827113f4ab59306ee8abbf0215e
gdianesi <gd329180@fi365.ort.edu.uy>
2025-09-24 19:28:35 -0300

    RED: event name must return an error due to invalidity

--- tests/VirtualPark.BusinessLogic.Test/Events/Modules/EventsArgsTest.cs
+++ tests/VirtualPark.BusinessLogic.Test/Events/Modules/EventsArgsTest.cs
@@ -16,4 +16,19 @@ public class EventsArgsTest
         eventsArgs.Name.Should().Be("Halloween");
     }
     #endregion
+
+    #region Failure
+    [TestMethod]
+    [DataRow(null)]
+    [DataRow("")]
+    [DataRow(" ")]
+    public void Constructor_WithInvalidName_ThrowsArgumentException(string name)
+    {
+        var act = () => new EventsArgs(name);
+
+        act.Should()
+            .Throw<ArgumentException>()
+            .WithMessage("Invalid event name");
+    }
+    #endregion
 }
