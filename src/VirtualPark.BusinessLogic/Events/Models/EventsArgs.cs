5fa15602a7b3bf397b55662a911e3fe1d9b5d9ea
gdianesi <gd329180@fi365.ort.edu.uy>
2025-09-24 19:42:27 -0300

    GREEN: event name must return an error due to invalidity

--- src/VirtualPark.BusinessLogic/Events/Models/EventsArgs.cs
+++ src/VirtualPark.BusinessLogic/Events/Models/EventsArgs.cs
@@ -1,3 +1,16 @@
 namespace VirtualPark.BusinessLogic.Events.Models;
 
-public sealed record EventsArgs(string Name);
+public sealed class EventsArgs
+{
+    public string Name { get; init; }
+
+    public EventsArgs(string name)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+        {
+            throw new ArgumentException("Invalid event name");
+        }
+
+        Name = name;
+    }
+}
