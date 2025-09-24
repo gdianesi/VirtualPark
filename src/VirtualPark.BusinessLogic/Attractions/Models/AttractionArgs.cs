4e7eb2a2436c4090bf8a76c3f4397742cb911f5f
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-09-24 18:19:51 -0300

    REFACTOR: Type_Getter_ReturnsAssignedValue

--- src/VirtualPark.BusinessLogic/Attractions/Models/AttractionArgs.cs
+++ src/VirtualPark.BusinessLogic/Attractions/Models/AttractionArgs.cs
@@ -1,6 +1,3 @@
 namespace VirtualPark.BusinessLogic.Attractions.Models;
 
-public class AttractionArgs(string type)
-{
-    public string Type { get; init; } = type;
-}
+public sealed record AttractionArgs(string Type) { }
