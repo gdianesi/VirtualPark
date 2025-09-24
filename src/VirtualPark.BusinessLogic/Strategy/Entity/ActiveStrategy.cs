6325f17e4c93ffaf15646c1cf3f6896cd7774144
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-09-23 21:17:10 -0300

    GREEN: StrategyKey_GetStrategyKey

--- src/VirtualPark.BusinessLogic/Strategy/Entity/ActiveStrategy.cs
+++ src/VirtualPark.BusinessLogic/Strategy/Entity/ActiveStrategy.cs
@@ -3,4 +3,5 @@ namespace VirtualPark.BusinessLogic.Strategy.Entity;
 public sealed class ActiveStrategy
 {
     public Guid Id { get; init; } = Guid.NewGuid();
+    public string StrategyKey { get; init; } = null!;
 }
