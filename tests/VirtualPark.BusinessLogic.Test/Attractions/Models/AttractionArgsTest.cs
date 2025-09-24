816977162fb1072efd1a9db60d728ab3dce7a155
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-09-24 18:26:09 -0300

    RED: MiniumAge_Getter_ReturnsAssignedValue

--- tests/VirtualPark.BusinessLogic.Test/Attractions/Models/AttractionArgsTest.cs
+++ tests/VirtualPark.BusinessLogic.Test/Attractions/Models/AttractionArgsTest.cs
@@ -20,13 +20,22 @@ public class AttractionArgsTest
     #endregion
 
     #region Name
-
     [TestMethod]
     [TestCategory("Validation")]
     public void Name_Getter_ReturnsAssignedValue()
     {
         var attractionArgs = new AttractionArgs("RollerCoaster", "The Big Bang");
         attractionArgs.Name.Should().Be("The Big Bang");
+    }
+    #endregion
+    #region MinumAge
+
+    [TestMethod]
+    [TestCategory("Validation")]
+    public void MiniumAge_Getter_ReturnsAssignedValue()
+    {
+        var attractionArgs = new AttractionArgs("RollerCoaster", "The Big Bang", 12);
+        attractionArgs.MiniumAge.Should().Be(18);
     }
     #endregion
 }
