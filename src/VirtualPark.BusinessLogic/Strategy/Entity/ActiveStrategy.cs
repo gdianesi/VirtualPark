5e5aa66edc2cb49593fd68497c74d121cb30e068
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-09-23 21:20:39 -0300

    GREEN: Date_GetSet_Works

--- src/VirtualPark.BusinessLogic/Strategy/Entity/ActiveStrategy.cs
+++ src/VirtualPark.BusinessLogic/Strategy/Entity/ActiveStrategy.cs
@@ -4,4 +4,5 @@ public sealed class ActiveStrategy
 {
     public Guid Id { get; init; } = Guid.NewGuid();
     public string StrategyKey { get; init; } = null!;
+    public DateTime Date { get; set; }
 }
