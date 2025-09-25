542bafc6dee5a488a997d49530404d53fba32b11
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-09-24 22:57:09 -0300

    Initialize attraction test

--- tests/VirtualPark.BusinessLogic.Test/Attractions/Services/AttractionServiceTest.cs
+++ tests/VirtualPark.BusinessLogic.Test/Attractions/Services/AttractionServiceTest.cs
@@ -1,3 +1,9 @@
+using FluentAssertions;
+using Moq;
+using VirtualPark.BusinessLogic.Attractions.Entity;
+using VirtualPark.BusinessLogic.Attractions.Services;
+using VirtualPark.Repository;
+
 namespace VirtualPark.BusinessLogic.Test.Attractions.Services;
 
 [TestClass]
@@ -5,4 +11,13 @@ namespace VirtualPark.BusinessLogic.Test.Attractions.Services;
 [TestCategory("AttractionServic")]
 public class AttractionServiceTest
 {
+    private Mock<IRepository<Attraction>> _mockAttractionRepository = null!;
+    private AttractionService _attractionService = null!;
+    
+    [TestInitialize]
+    public void Initialize()
+    {
+        _mockAttractionRepository = new Mock<IRepository<Attraction>>(MockBehavior.Strict);
+        _attractionService = new AttractionService(_mockAttractionRepository.Object);
+    }
 }
