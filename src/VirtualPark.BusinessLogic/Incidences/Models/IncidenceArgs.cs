d9844088e5009e6561f8d64e9dc5d034ced8c0d3
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-09-27 19:01:23 -0300

    GREEN: Description_ShouldParseStringGuid_ToGuidProperty

--- src/VirtualPark.BusinessLogic/Incidences/Models/IncidenceArgs.cs
+++ src/VirtualPark.BusinessLogic/Incidences/Models/IncidenceArgs.cs
@@ -2,7 +2,8 @@ using VirtualPark.BusinessLogic.Validations.Services;
 
 namespace VirtualPark.BusinessLogic.Incidences.Models;
 
-public sealed class IncidenceArgs(string typeIncidence)
+public sealed class IncidenceArgs(string typeIncidence, string description)
 {
     public Guid TypeIncidence { get; init; } = ValidationServices.ValidateAndParseGuid(typeIncidence);
+    public string Description { get; set; } = ValidationServices.ValidateNullOrEmpty(description);
 }
--- tests/VirtualPark.BusinessLogic.Test/Incidences/Models/IncidenceArgsTest.cs
+++ tests/VirtualPark.BusinessLogic.Test/Incidences/Models/IncidenceArgsTest.cs
@@ -13,9 +13,7 @@ public class IncidenceArgsTest
     public void TypeIncidence_ShouldParseStringGuid_ToGuidProperty()
     {
         Guid expectedId = Guid.Parse("c8a0b0ef-9a4d-46e0-b9d3-0dfd68b6a010");
-
-        var incidenceArgs = new IncidenceArgs("c8a0b0ef-9a4d-46e0-b9d3-0dfd68b6a010");
-
+        var incidenceArgs = new IncidenceArgs("c8a0b0ef-9a4d-46e0-b9d3-0dfd68b6a010", "Description");
         incidenceArgs.TypeIncidence.Should().Be(expectedId);
     }
     #endregion
@@ -25,7 +23,7 @@ public class IncidenceArgsTest
     public void Description_ShouldParseStringGuid_ToGuidProperty()
     {
         var incidenceArgs = new IncidenceArgs("c8a0b0ef-9a4d-46e0-b9d3-0dfd68b6a010", "Description");
-        incidenceArgs.Description.Sholud().Be("Description");
+        incidenceArgs.Description.Should().Be("Description");
     }
     #endregion
 }
