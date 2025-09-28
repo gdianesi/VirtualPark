c29475e8630ca2a7877dc98cce95d04eadf25148
Alfonso Ricaldoni <AR287195@fi365.ort.edu.uy>
2025-09-27 23:36:44 -0300

    fix referens error

--- tests/VirtualPark.BusinessLogic.Test/VisitorsProfile/Models/VisitorProfileArgsTest.cs
+++ tests/VirtualPark.BusinessLogic.Test/VisitorsProfile/Models/VisitorProfileArgsTest.cs
@@ -1,4 +1,5 @@
 using FluentAssertions;
+using VirtualPark.BusinessLogic.VisitorsProfile.Entity;
 using VirtualPark.BusinessLogic.VisitorsProfile.Models;
 
 namespace VirtualPark.BusinessLogic.Test.VisitorsProfile.Models;
