d02a450b3e76ec08370ca7028dea588bed7af9e8
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-09-27 22:33:22 -0300

    GREEN: End_ShouldParseStringDateTime_ToDateTimeProperty

--- src/VirtualPark.BusinessLogic/Incidences/Models/IncidenceArgs.cs
+++ src/VirtualPark.BusinessLogic/Incidences/Models/IncidenceArgs.cs
@@ -2,9 +2,10 @@ using VirtualPark.BusinessLogic.Validations.Services;
 
 namespace VirtualPark.BusinessLogic.Incidences.Models;
 
-public sealed class IncidenceArgs(string typeIncidence, string description, string start)
+public sealed class IncidenceArgs(string typeIncidence, string description, string start, string end)
 {
     public Guid TypeIncidence { get; init; } = ValidationServices.ValidateAndParseGuid(typeIncidence);
     public string Description { get; init; } = ValidationServices.ValidateNullOrEmpty(description);
     public DateTime Start { get; init; } = ValidationServices.ValidateDateTime(start);
+    public DateTime End { get; set; } = ValidationServices.ValidateDateTime(end);
 }
--- tests/VirtualPark.BusinessLogic.Test/Incidences/Models/IncidenceArgsTest.cs
+++ tests/VirtualPark.BusinessLogic.Test/Incidences/Models/IncidenceArgsTest.cs
@@ -16,8 +16,10 @@ public class IncidenceArgsTest
         var incidenceArgs = new IncidenceArgs(
             "c8a0b0ef-9a4d-46e0-b9d3-0dfd68b6a010",
             "Description",
+            "2025-09-27 15:30",
             "2025-09-27 15:30"
-        );        incidenceArgs.TypeIncidence.Should().Be(expectedId);
+        );
+        incidenceArgs.TypeIncidence.Should().Be(expectedId);
     }
     #endregion
     #region Description
@@ -28,6 +30,7 @@ public class IncidenceArgsTest
         var incidenceArgs = new IncidenceArgs(
             "c8a0b0ef-9a4d-46e0-b9d3-0dfd68b6a010",
             "Description",
+            "2025-09-27 15:30",
             "2025-09-27 15:30"
         );
         incidenceArgs.Description.Should().Be("Description");
@@ -38,10 +41,10 @@ public class IncidenceArgsTest
     public void Start_ShouldParseStringDateTime_ToDateTimeProperty()
     {
         var expected = new DateTime(2025, 9, 27, 15, 30, 0);
-
         var incidenceArgs = new IncidenceArgs(
             "c8a0b0ef-9a4d-46e0-b9d3-0dfd68b6a010",
             "Description",
+            "2025-09-27 15:30",
             "2025-09-27 15:30"
         );
         incidenceArgs.Start.Should().Be(expected);
