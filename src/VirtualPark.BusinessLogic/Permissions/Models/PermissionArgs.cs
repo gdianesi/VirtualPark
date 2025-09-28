2e1f2617c4c45fd3d9c453af9948eedc1c601402
gdianesi <gd329180@fi365.ort.edu.uy>
2025-09-28 19:48:10 -0300

    RED: when key is null or empty should throw arg. excp

--- src/VirtualPark.BusinessLogic/Permissions/Models/PermissionArgs.cs
+++ src/VirtualPark.BusinessLogic/Permissions/Models/PermissionArgs.cs
@@ -5,6 +5,16 @@ namespace VirtualPark.BusinessLogic.Permissions.Models;
 public sealed class PermissionArgs(string description, string key, List<Guid> roles)
 {
     public string Description { get; set; } = ValidationServices.ValidateNullOrEmpty(description);
-    public string Key { get; set; } = key;
+    public string Key { get; set; } = ValidateNullOrEmpty(key);
     public List<Guid> Roles { get; set; } = roles;
+
+    public static string ValidateNullOrEmpty(string value)
+    {
+        if(string.IsNullOrWhiteSpace(value))
+        {
+            throw new ArgumentException("Value cannot be null or empty.");
+        }
+
+        return value;
+    }
 }
