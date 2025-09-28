aaa54e224b3f56f27018ce9811985b3ac6067f5f
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-09-27 22:06:25 -0300

    RED: Start_ShouldParseStringDateTime_ToDateTimeProperty

--- tests/VirtualPark.BusinessLogic.Test/Incidences/Models/IncidenceArgsTest.cs
+++ tests/VirtualPark.BusinessLogic.Test/Incidences/Models/IncidenceArgsTest.cs
@@ -22,8 +22,21 @@ public class IncidenceArgsTest
     [TestMethod]
     public void Description_ShouldParseStringGuid_ToGuidProperty()
     {
-        var incidenceArgs = new IncidenceArgs("c8a0b0ef-9a4d-46e0-b9d3-0dfd68b6a010", "Description");
+        var incidenceArgs = new IncidenceArgs("c8a0b0ef-9a4d-46e0-b9d3-0dfd68b6a010", "Description", "27/09/2025 15:30");
         incidenceArgs.Description.Should().Be("Description");
+    }
+    #endregion
+    #region Start
+
+    [TestMethod]
+    [TestCategory("Incidences")]
+    public void Start_ShouldParseStringDateTime_ToDateTimeProperty()
+    {
+        var expected = new DateTime(2025, 9, 27, 15, 30, 0); 
+
+        var incidenceArgs = new IncidenceArgs("c8a0b0ef-9a4d-46e0-b9d3-0dfd68b6a010", "Description", "27/09/2025 15:30");
+
+        incidenceArgs.Start.Should().Be(expected);
     }
     #endregion
 }
