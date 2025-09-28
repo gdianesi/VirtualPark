64fe26cd930b1d1ae503a04e5f76c314bd89826d
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-09-27 22:47:55 -0300

    RED: Active_ShouldParseStringDateTime_ToDateTimeProperty

--- tests/VirtualPark.BusinessLogic.Test/Incidences/Models/IncidenceArgsTest.cs
+++ tests/VirtualPark.BusinessLogic.Test/Incidences/Models/IncidenceArgsTest.cs
@@ -70,7 +70,6 @@ public class IncidenceArgsTest
     }
     #endregion
     #region Id
-
     [TestMethod]
     public void IdAttraction_ShouldParseStringGuid_ToGuidProperty()
     {
@@ -83,6 +82,22 @@ public class IncidenceArgsTest
             "c8a0b0ef-9a4d-46e0-b9d3-0dfd68b6a010"
         );
         incidenceArgs.AttractionId.Should().Be(expectedId);
+    }
+    #endregion
+    #region Active
+
+    [TestMethod]
+    public void Active_ShouldParseStringDateTime_ToDateTimeProperty()
+    {
+        var incidenceArgs = new IncidenceArgs(
+            "c8a0b0ef-9a4d-46e0-b9d3-0dfd68b6a010",
+            "Description",
+            "2025-09-27 15:30",
+            "2025-09-27 15:30",
+            "c8a0b0ef-9a4d-46e0-b9d3-0dfd68b6a010",
+            "true"
+        );
+        incidenceArgs.Active.Should().BeTrue();
     }
     #endregion
 }
