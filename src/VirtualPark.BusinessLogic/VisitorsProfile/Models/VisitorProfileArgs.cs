8c33bcee3779cb316a8933a0cc3666abbbe39c01
Alfonso Ricaldoni <AR287195@fi365.ort.edu.uy>
2025-09-28 19:06:35 -0300

    REFACTOR: VisitorProfileArgs

--- src/VirtualPark.BusinessLogic/VisitorsProfile/Models/VisitorProfileArgs.cs
+++ src/VirtualPark.BusinessLogic/VisitorsProfile/Models/VisitorProfileArgs.cs
@@ -6,18 +6,5 @@ namespace VirtualPark.BusinessLogic.VisitorsProfile.Models;
 public class VisitorProfileArgs(string dateOfBirth, string membership)
 {
     public DateOnly DateOfBirth { get; init; } = ValidationServices.ParseDateOfBirth(dateOfBirth);
-    public Membership Membership { get; init; } = ParseMembership(membership);
-
-    private static Membership ParseMembership(string membership)
-    {
-        var isNotValid = !Enum.TryParse<Membership>(membership, true, out var parsedMembership);
-        if(isNotValid)
-        {
-            throw new ArgumentException(
-                $"Invalid membership value: {membership}",
-                nameof(membership));
-        }
-
-        return parsedMembership;
-    }
+    public Membership Membership { get; init; } = ValidationServices.ParseMembership(membership);
 }
