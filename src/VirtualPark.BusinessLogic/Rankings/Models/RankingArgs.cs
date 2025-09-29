ab12514ded6cffdc5e5200c0c6977e93f2170d16
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-09-29 20:07:15 -0300

    REFACOTR: list entries

--- src/VirtualPark.BusinessLogic/Rankings/Models/RankingArgs.cs
+++ src/VirtualPark.BusinessLogic/Rankings/Models/RankingArgs.cs
@@ -1,8 +1,10 @@
+using VirtualPark.BusinessLogic.Users.Entity;
 using VirtualPark.BusinessLogic.Validations.Services;
 
 namespace VirtualPark.BusinessLogic.Rankings.Models;
 
-public sealed class RankingArgs(string date)
+public sealed class RankingArgs(string date, string[] entries)
 {
     public DateTime Date { get; init; } = ValidationServices.ValidateDateTime(date);
+    public List<Guid> Entries { get; init; } = entries.Select(ValidationServices.ValidateAndParseGuid).ToList();
 }
--- tests/VirtualPark.BusinessLogic.Test/Rankings/Models/RankingArgsTest.cs
+++ tests/VirtualPark.BusinessLogic.Test/Rankings/Models/RankingArgsTest.cs
@@ -13,8 +13,39 @@ public class RankingArgsTest
     public void Date_Getter_ReturnsAssignedValue()
     {
         var expected = new DateTime(2025, 9, 27, 00, 00, 00);
-        var rankingArgs = new RankingArgs("2025-09-27 00:00");
+        var rankingArgs = new RankingArgs("2025-09-27 00:00", Array.Empty<string>());
         rankingArgs.Date.Should().Be(expected);
     }
     #endregion Date
+    #region Users
+
+    [TestMethod]
+    public void Entries_Getter_ReturnsValueAfterSet()
+    {
+        var rankingArgs = new RankingArgs("2025-09-27 00:00", Array.Empty<string>());
+        var expected = new List<Guid> { Guid.NewGuid(), Guid.NewGuid() };
+
+        rankingArgs.Entries = expected;
+
+        rankingArgs.Entries.Should().BeSameAs(expected);
+        rankingArgs.Entries.Should().HaveCount(2);
+    }
+
+    [TestMethod]
+    public void Entries_Setter_ReplacesPreviousList()
+    {
+        var rankingArgs = new RankingArgs("2025-09-27 00:00", Array.Empty<string>());
+        var first = new List<Guid> { Guid.NewGuid() };
+        var second = new List<Guid> { Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid() };
+
+        rankingArgs.Entries = first;
+        rankingArgs.Entries = second;
+
+        rankingArgs.Entries.Should().BeSameAs(second);
+        rankingArgs.Entries.Should().NotBeSameAs(first);
+        rankingArgs.Entries.Should().HaveCount(3);
+    }
+
+    #endregion Users
+
 }
