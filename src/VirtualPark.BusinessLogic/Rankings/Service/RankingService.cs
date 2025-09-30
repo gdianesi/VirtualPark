3d3318ad54c208db491ed9282dcfce54815c4122
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-09-30 18:58:05 -0300

    RED: guid to user

--- src/VirtualPark.BusinessLogic/Rankings/Service/RankingService.cs
+++ src/VirtualPark.BusinessLogic/Rankings/Service/RankingService.cs
@@ -1,6 +1,13 @@
+using VirtualPark.BusinessLogic.Rankings.Entity;
+using VirtualPark.BusinessLogic.Rankings.Models;
+using VirtualPark.BusinessLogic.Users.Entity;
+using VirtualPark.Repository;
+
 namespace VirtualPark.BusinessLogic.Rankings.Service;
 
-public class RankingService
+public sealed class RankingService(IRepository<Ranking> rankingRepository, IReadOnlyRepository<User> userReadOnlyRepository)
 {
+    private readonly IRepository<Ranking> _rankingRepository = rankingRepository;
+    private readonly IReadOnlyRepository<User> _userReadOnlyRepository = userReadOnlyRepository;
     
 }
--- tests/VirtualPark.BusinessLogic.Test/Rankings/Service/RankingServiceTest.cs
+++ tests/VirtualPark.BusinessLogic.Test/Rankings/Service/RankingServiceTest.cs
@@ -1,6 +1,53 @@
+using FluentAssertions;
+using Moq;
+using VirtualPark.BusinessLogic.Rankings.Entity; using VirtualPark.BusinessLogic.Rankings.Service;
+using VirtualPark.BusinessLogic.Users.Entity;
+using VirtualPark.Repository;
+
 namespace VirtualPark.BusinessLogic.Test.Rankings.Service;
 
-public class RankingServiceTest
+[TestClass]
+[TestCategory("Rankings")]
+public sealed class RankingServiceTest
 {
-    
+    private Mock<IRepository<Ranking>> _mockRankingRepository = null!;
+    private Mock<IReadOnlyRepository<User>> _mockUserReadOnlyRepository = null!;
+    private RankingService _rankingService = null!;
+
+    [TestInitialize]
+    public void Initialize()
+    {
+        _mockRankingRepository = new Mock<IRepository<Ranking>>(MockBehavior.Strict);
+        _mockUserReadOnlyRepository = new Mock<IReadOnlyRepository<User>>(MockBehavior.Strict);
+
+        _rankingService = new RankingService(
+            _mockRankingRepository.Object,
+            _mockUserReadOnlyRepository.Object
+        );
+    }
+
+    #region GuidToUser
+    [TestMethod]
+    public void GuidToUser_WhenUserDoesNotExist_ShouldThrowKeyNotFound()
+    {
+        var g1 = Guid.NewGuid();
+        _mockUserReadOnlyRepository.Setup(r => r.Get(u => u.Id == g1)).Returns((User?)null);
+
+        var entries = new List<Guid> { g1 };
+
+        Action act = () => _rankingService.GuidToUser(entries);
+
+        act.Should().Throw<KeyNotFoundException>()
+            .WithMessage($"User with id {g1} does not exist");
+    }
+
+    [TestMethod]
+    public void GuidToUser_WhenEntriesIsNull_ShouldThrowArgumentNull()
+    {
+        Action act = () => _rankingService.GuidToUser(null!);
+
+        act.Should().Throw<ArgumentNullException>();
+    }
+
+    #endregion
 }
