2e695d45a18d36925da08ac1df33df24285d6a7e
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-10-01 19:55:00 -0300

    RED: PermissionIds_Getter_ReturnsParsedGuids

--- tests/VirtualPark.BusinessLogic.Test/Roles/Models/RoleArgsTest.cs
+++ tests/VirtualPark.BusinessLogic.Test/Roles/Models/RoleArgsTest.cs
@@ -12,17 +12,44 @@ public sealed class RoleArgsTest
     [TestMethod]
     public void Name_Getter_ReturnsAssignedValue()
     {
-        var roleArgs = new RoleArgs("Visitor", "Description");
+        var roleArgs = new RoleArgs("Visitor", "Description", Array.Empty<string>());
         roleArgs.Name.Should().Be("Visitor");
     }
     #endregion
-    #region Description
 
+    #region Description
     [TestMethod]
     public void Description_Getter_ReturnsAssignedValue()
     {
-        var roleArgs = new RoleArgs("Visitor", "Description");
+        var roleArgs = new RoleArgs("Visitor", "Description", Array.Empty<string>());
         roleArgs.Description.Should().Be("Description");
     }
+    #endregion
+
+    #region PermissionIds
+    [TestMethod]
+    public void PermissionIds_Getter_ReturnsParsedGuids()
+    {
+        var g1 = Guid.NewGuid();
+        var g2 = Guid.NewGuid();
+        var permissions = new[] { g1.ToString(), g2.ToString() };
+
+        var roleArgs = new RoleArgs("Visitor", "Description", permissions);
+
+        roleArgs.PermissionIds.Should().HaveCount(2);
+        roleArgs.PermissionIds.Should().ContainInOrder(g1, g2);
+    }
+
+    [TestMethod]
+    public void PermissionIds_WhenInvalidGuid_ShouldThrowFormatException()
+    {
+        var invalidPermissions = new[] { "not-a-guid" };
+
+        FluentAssertions.FluentActions
+            .Invoking(() => new RoleArgs("Visitor", "Description", invalidPermissions))
+            .Should().Throw<FormatException>()
+            .WithMessage("The value 'not-a-guid' is not a valid Guid.");
+    }
+
     #endregion
 }
