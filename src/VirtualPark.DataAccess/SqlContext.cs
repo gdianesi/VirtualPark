6eab13ad4df5190ec0f6c153ddb0675877ee5c22
Alfonso Ricaldoni <AR287195@fi365.ort.edu.uy>
2025-10-02 18:00:03 -0300

    commit to change brancg

--- src/VirtualPark.DataAccess/SqlContext.cs
+++ src/VirtualPark.DataAccess/SqlContext.cs
@@ -15,5 +15,43 @@ public class SqlContext(DbContextOptions<SqlContext> options) : DbContext(option
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.ToTable("Users");
+            entity.HasKey(e => e.Id);
+
+            entity.Property(e => e.Name).IsRequired();
+            entity.Property(e => e.LastName).IsRequired();
+            entity.Property(e => e.Email).IsRequired();
+            entity.Property(e => e.Password).IsRequired();
+
+            entity.HasIndex(e => e.Email).IsUnique();
+
+            entity.HasOne(e => e.VisitorProfile)
+                .WithOne()
+                .HasForeignKey<User>(e => e.VisitorProfileId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            entity.HasMany(e => e.Roles)
+                .WithMany()
+                .UsingEntity<Dictionary<string, object>>(
+                    "UserRoles",
+                    j => j.HasOne<Role>()
+                        .WithMany()
+                        .HasForeignKey("RoleId")
+                        .OnDelete(DeleteBehavior.Restrict),
+                    j => j.HasOne<User>()
+                        .WithMany()
+                        .HasForeignKey("UserId")
+                        .OnDelete(DeleteBehavior.Restrict),
+                    j =>
+                    {
+                        j.ToTable("UserRoles");
+                        j.HasKey("UserId", "RoleId");
+                        j.HasIndex("RoleId");
+                    });
+        });
     }
 }
