a81435e24c41afd9b139ce668d319aa0578101c1
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-10-02 18:47:23 -0300

    GREEN: OffsetMinutes_GetAfterSet_ShouldReturnSameValue

--- src/VirtualPark.BusinessLogic/ClocksApp/Entity/ClockApp.cs
+++ src/VirtualPark.BusinessLogic/ClocksApp/Entity/ClockApp.cs
@@ -2,5 +2,6 @@ namespace VirtualPark.BusinessLogic.ClocksApp.Entity;
 
 public sealed class ClockApp
 {
-    public Guid Id { get; init; } = Guid.NewGuid(); 
+    public Guid Id { get; init; } = Guid.NewGuid();
+    public int OffsetMinutes { get; set; } = 0;
 }
