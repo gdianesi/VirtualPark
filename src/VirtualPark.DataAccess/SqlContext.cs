9c933f91820141a3adb4732406a678af962a4cf6
Alfonso Ricaldoni <AR287195@fi365.ort.edu.uy>
2025-10-02 21:31:18 -0300

    add user entity to sql context

--- src/VirtualPark.DataAccess/SqlContext.cs
+++ src/VirtualPark.DataAccess/SqlContext.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using VirtualPark.BusinessLogic.Permissions.Entity;
 using VirtualPark.BusinessLogic.Roles.Entity;
+using VirtualPark.BusinessLogic.UserRoles.Entity;
 using VirtualPark.BusinessLogic.Users.Entity;
 using VirtualPark.BusinessLogic.VisitorsProfile.Entity;
 
@@ -20,37 +21,41 @@ public class SqlContext(DbContextOptions<SqlContext> options) : DbContext(option
         modelBuilder.Entity<User>(entity =>
         {
             entity.ToTable("Users");
-            entity.HasKey(e => e.Id);
 
-            entity.Property(e => e.Name).IsRequired();
-            entity.Property(e => e.LastName).IsRequired();
-            entity.Property(e => e.Email).IsRequired();
-            entity.Property(e => e.Password).IsRequired();
+            entity.HasKey(u => u.Id);
+            entity.Property(u => u.Id).ValueGeneratedNever();
 
-            entity.HasIndex(e => e.Email).IsUnique();
+            entity.Property(u => u.Name).IsRequired();
+            entity.Property(u => u.LastName).IsRequired();
+            entity.Property(u => u.Email).IsRequired();
+            entity.Property(u => u.Password).IsRequired();
 
-            entity.HasOne(e => e.VisitorProfile)
-                .WithOne()
-                .HasForeignKey<User>(e => e.VisitorProfileId)
+            entity.HasIndex(u => u.Email).IsUnique();
+
+            entity
+                .HasOne(u => u.VisitorProfile)
+                .WithMany()
+                .HasForeignKey(u => u.VisitorProfileId)
                 .OnDelete(DeleteBehavior.Restrict);
 
-            entity.HasMany(e => e.Roles)
-                .WithMany()
-                .UsingEntity<Dictionary<string, object>>(
-                    "UserRoles",
-                    j => j.HasOne<Role>()
-                        .WithMany()
-                        .HasForeignKey("RoleId")
-                        .OnDelete(DeleteBehavior.Restrict),
-                    j => j.HasOne<User>()
-                        .WithMany()
-                        .HasForeignKey("UserId")
-                        .OnDelete(DeleteBehavior.Restrict),
+            entity
+                .HasMany(u => u.Roles)
+                .WithMany(r => r.Users)
+                .UsingEntity<UserRole>(
+                    j =>
+                        j.HasOne<Role>()
+                            .WithMany()
+                            .HasForeignKey(ur => ur.RoleId)
+                            .OnDelete(DeleteBehavior.Restrict),
+                    j =>
+                        j.HasOne<User>()
+                            .WithMany()
+                            .HasForeignKey(ur => ur.UserId)
+                            .OnDelete(DeleteBehavior.Restrict),
                     j =>
                     {
                         j.ToTable("UserRoles");
-                        j.HasKey("UserId", "RoleId");
-                        j.HasIndex("RoleId");
+                        j.HasKey(ur => new { ur.UserId, ur.RoleId });
                     });
         });
     }
