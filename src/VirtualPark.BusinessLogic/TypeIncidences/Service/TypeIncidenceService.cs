40a9922036c1664308af2287504dae0093798c03
Alfonso Ricaldoni <AR287195@fi365.ort.edu.uy>
2025-10-04 14:55:12 -0300

    REFACTOR: create of TypeIncidence

--- src/VirtualPark.BusinessLogic/TypeIncidences/Service/TypeIncidenceService.cs
+++ src/VirtualPark.BusinessLogic/TypeIncidences/Service/TypeIncidenceService.cs
@@ -16,24 +16,24 @@ public sealed class TypeIncidenceService(IRepository<TypeIncidence> typeIncidenc
         return typeIncidence.Id;
     }
 
-    public List<TypeIncidence> GetAll(Expression<Func<TypeIncidence, bool>>? predicate = null)
+    public List<TypeIncidence> GetAll()
     {
-        return _typeIncidenceRepository.GetAll(predicate);
+        return _typeIncidenceRepository.GetAll();
     }
 
-    public TypeIncidence Get(Expression<Func<TypeIncidence, bool>> predicate)
+    public TypeIncidence Get(Guid id)
     {
-        return _typeIncidenceRepository.Get(predicate);
+        return _typeIncidenceRepository.Get(t => t.Id == id);
     }
 
-    public bool Exist(Expression<Func<TypeIncidence, bool>> predicate)
+    private bool Exist(Expression<Func<TypeIncidence, bool>> predicate)
     {
         return _typeIncidenceRepository.Exist(predicate);
     }
 
     public void Update(Guid id, TypeIncidenceArgs args)
     {
-        TypeIncidence typeIncidence = Get(t => t.Id == id) ?? throw new InvalidOperationException($"TypeIncidence with id {id} not found.");
+        TypeIncidence typeIncidence = Get(id) ?? throw new InvalidOperationException($"TypeIncidence with id {id} not found.");
         ApplyArgsToEntity(typeIncidence, args);
         _typeIncidenceRepository.Update(typeIncidence);
     }
--- tests/VirtualPark.BusinessLogic.Test/TypeIncidences/Service/TypeIncidenceServiceTest.cs
+++ tests/VirtualPark.BusinessLogic.Test/TypeIncidences/Service/TypeIncidenceServiceTest.cs
@@ -25,14 +25,13 @@ public class TypeIncidenceServiceTest
     }
 
     #region Create
-
     [TestMethod]
     public void Create_WhenArgsAreValid_ShouldReturnNewGuid_AndAddEntityWithSameId()
     {
         TypeIncidence? captured = null;
 
         _mockTypeIncidenceRepository
-            .Setup(r => r.Add(It.IsAny<TypeIncidence>()))
+            .Setup(r => r.Add(It.Is<TypeIncidence>(ti => ti.Type == _typeIncidenceArgs.Type)))
             .Callback<TypeIncidence>(ti => captured = ti);
 
         Guid id = _typeIncidenceService.Create(_typeIncidenceArgs);
@@ -42,11 +41,10 @@ public class TypeIncidenceServiceTest
         captured!.Id.Should().Be(id);
         captured.Type.Should().Be(_typeIncidenceArgs.Type);
 
-        _mockTypeIncidenceRepository.Verify(r => r.Add(It.IsAny<TypeIncidence>()), Times.Once);
         _mockTypeIncidenceRepository.VerifyAll();
     }
-
     #endregion
+
     #region MapToEntity
 
     [TestMethod]
@@ -58,6 +56,7 @@ public class TypeIncidenceServiceTest
         typeIncidence.Type.Should().Be(_typeIncidenceArgs.Type);
     }
     #endregion
+
     #region GetAll
     [TestMethod]
     public void GetAll_WhenPredicateProvided_ShouldReturnFiltered_AndCallWithPredicate()
@@ -76,7 +75,7 @@ public class TypeIncidenceServiceTest
 
         Expression<Func<TypeIncidence, bool>> predicate = t => t.Type == "Locked";
 
-        var result = _typeIncidenceService.GetAll(predicate);
+        var result = _typeIncidenceService.GetAll();
 
         result.Should().HaveCount(2);
         result.All(t => t.Type == "Locked").Should().BeTrue();
@@ -107,18 +106,18 @@ public class TypeIncidenceServiceTest
     }
 
     #endregion
+
     #region Get
     [TestMethod]
     public void Get_WhenEntityExists_ShouldReturnEntity()
     {
         var expected = new TypeIncidence { Id = Guid.NewGuid(), Type = "Locked" };
-        Expression<Func<TypeIncidence, bool>> predicate = x => x.Type == "Locked";
 
         _mockTypeIncidenceRepository
             .Setup(r => r.Get(It.IsAny<Expression<Func<TypeIncidence, bool>>>()))
             .Returns(expected);
 
-        var result = _typeIncidenceService.Get(predicate);
+        var result = _typeIncidenceService.Get(expected.Id);
 
         result.Should().NotBeNull();
         result.Should().BeSameAs(expected);
@@ -131,13 +130,11 @@ public class TypeIncidenceServiceTest
     [TestMethod]
     public void Get_WhenEntityDoesNotExist_ShouldReturnNull()
     {
-        Expression<Func<TypeIncidence, bool>> predicate = x => x.Type == "Missing";
-
         _mockTypeIncidenceRepository
             .Setup(r => r.Get(It.IsAny<Expression<Func<TypeIncidence, bool>>>()))
             .Returns((TypeIncidence?)null);
 
-        var result = _typeIncidenceService.Get(predicate);
+        var result = _typeIncidenceService.Get(Guid.NewGuid());
 
         result.Should().BeNull();
 
@@ -145,60 +142,7 @@ public class TypeIncidenceServiceTest
         _mockTypeIncidenceRepository.VerifyAll();
     }
     #endregion
-    #region Exist
 
-    [TestMethod]
-    public void Exist_WhenRepositoryReturnsTrue_ShouldReturnTrue()
-    {
-        Expression<Func<TypeIncidence, bool>> predicate = t => t.Type == "Locked";
-
-        _mockTypeIncidenceRepository
-            .Setup(r => r.Exist(It.IsAny<Expression<Func<TypeIncidence, bool>>>()))
-            .Returns(true);
-
-        var exists = _typeIncidenceService.Exist(predicate);
-
-        exists.Should().BeTrue();
-        _mockTypeIncidenceRepository.Verify(
-            r => r.Exist(It.IsAny<Expression<Func<TypeIncidence, bool>>>()), Times.Once);
-        _mockTypeIncidenceRepository.VerifyAll();
-    }
-
-    [TestMethod]
-    public void Exist_WhenRepositoryReturnsFalse_ShouldReturnFalse()
-    {
-        Expression<Func<TypeIncidence, bool>> predicate = t => t.Type == "Missing";
-
-        _mockTypeIncidenceRepository
-            .Setup(r => r.Exist(It.IsAny<Expression<Func<TypeIncidence, bool>>>()))
-            .Returns(false);
-
-        var exists = _typeIncidenceService.Exist(predicate);
-
-        exists.Should().BeFalse();
-        _mockTypeIncidenceRepository.Verify(
-            r => r.Exist(It.IsAny<Expression<Func<TypeIncidence, bool>>>()), Times.Once);
-        _mockTypeIncidenceRepository.VerifyAll();
-    }
-
-    [TestMethod]
-    public void Exist_ShouldForwardSamePredicateToRepository()
-    {
-        Expression<Func<TypeIncidence, bool>> predicate = t => t.Type.StartsWith("Lo");
-
-        _mockTypeIncidenceRepository
-            .Setup(r => r.Exist(It.Is<Expression<Func<TypeIncidence, bool>>>(p => p == predicate)))
-            .Returns(true);
-
-        var exists = _typeIncidenceService.Exist(predicate);
-
-        exists.Should().BeTrue();
-        _mockTypeIncidenceRepository.Verify(
-            r => r.Exist(It.Is<Expression<Func<TypeIncidence, bool>>>(p => p == predicate)), Times.Once);
-        _mockTypeIncidenceRepository.VerifyAll();
-    }
-
-    #endregion
     #region ApplyArgsToEntity
     [TestMethod]
     public void ApplyArgsToEntity_ShouldCopyTypeFromArgs()
@@ -212,6 +156,7 @@ public class TypeIncidenceServiceTest
     }
 
     #endregion
+
     #region Update
 
     [TestMethod]
@@ -262,6 +207,7 @@ public class TypeIncidenceServiceTest
     }
 
     #endregion
+
     #region Delete
 
     [TestMethod]
@@ -309,5 +255,4 @@ public class TypeIncidenceServiceTest
     }
 
     #endregion
-
 }
