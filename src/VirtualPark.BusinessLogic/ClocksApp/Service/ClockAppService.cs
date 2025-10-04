9018a5b52b5a51a050bf319b08d47f7ad4a22bd3
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-10-03 21:02:26 -0300

    RED: Create_WhenArgsAreValid_ShouldPersistAndReturnId

--- src/VirtualPark.BusinessLogic/ClocksApp/Service/ClockAppService.cs
+++ src/VirtualPark.BusinessLogic/ClocksApp/Service/ClockAppService.cs
@@ -8,14 +8,7 @@ namespace VirtualPark.BusinessLogic.ClocksApp.Service;
 public sealed class ClockAppService(IRepository<ClockApp> clockAppRepository) : IClockAppService
 {
     private readonly IRepository<ClockApp> _clockAppRepository = clockAppRepository;
-
-    public Guid Create(ClockAppArgs clockAppArgs)
-    {
-        var clockApp = new ClockApp { DateSystem = clockAppArgs.SystemDateTime };
-        _clockAppRepository.Add(clockApp);
-        return clockApp.Id;
-    }
-
+    
     public DateTime Now()
     {
         throw new NotImplementedException();
