6561a0a8552495c5fd8bbdf430f9cf4c0049487e
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-10-05 19:34:58 -0300

    create user configuration

--- src/VirtualPark.DataAccess/SqlContext.cs
+++ src/VirtualPark.DataAccess/SqlContext.cs
@@ -12,6 +12,8 @@ using VirtualPark.BusinessLogic.TypeIncidences.Entity;
 using VirtualPark.BusinessLogic.UserRoles.Entity;
 using VirtualPark.BusinessLogic.Users.Entity;
 using VirtualPark.BusinessLogic.VisitorsProfile.Entity;
+using VirtualPark.BusinessLogic.VisitRegistrations.Entity;
+using VirtualPark.BusinessLogic.VisitsRegistrationsAttractions.Entity;
 
 namespace VirtualPark.DataAccess;
 
@@ -27,6 +29,7 @@ public class SqlContext(DbContextOptions<SqlContext> options) : DbContext(option
     public DbSet<Incidence> Incidences { get; set; }
     public DbSet<Ranking> Rankings { get; set; }
     public DbSet<Ticket> Tickets { get; set; }
+    public DbSet<VisitRegistration> VisitRegistrations { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -34,43 +37,7 @@ public class SqlContext(DbContextOptions<SqlContext> options) : DbContext(option
 
         modelBuilder.Entity<User>(entity =>
         {
-            entity.ToTable("Users");
-
-            entity.HasKey(u => u.Id);
-            entity.Property(u => u.Id).ValueGeneratedNever();
-
-            entity.Property(u => u.Name).IsRequired();
-            entity.Property(u => u.LastName).IsRequired();
-            entity.Property(u => u.Email).IsRequired();
-            entity.Property(u => u.Password).IsRequired();
-
-            entity.HasIndex(u => u.Email).IsUnique();
-
-            entity
-                .HasOne(u => u.VisitorProfile)
-                .WithMany()
-                .HasForeignKey(u => u.VisitorProfileId)
-                .OnDelete(DeleteBehavior.Restrict);
-
-            entity
-                .HasMany(u => u.Roles)
-                .WithMany(r => r.Users)
-                .UsingEntity<UserRole>(
-                    j =>
-                        j.HasOne<Role>()
-                            .WithMany()
-                            .HasForeignKey(ur => ur.RoleId)
-                            .OnDelete(DeleteBehavior.Restrict),
-                    j =>
-                        j.HasOne<User>()
-                            .WithMany()
-                            .HasForeignKey(ur => ur.UserId)
-                            .OnDelete(DeleteBehavior.Restrict),
-                    j =>
-                    {
-                        j.ToTable("UserRoles");
-                        j.HasKey(ur => new { ur.UserId, ur.RoleId });
-                    });
+            
         });
 
         modelBuilder.Entity<Role>(entity =>
