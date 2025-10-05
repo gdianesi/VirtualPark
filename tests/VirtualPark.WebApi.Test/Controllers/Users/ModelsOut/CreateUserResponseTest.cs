124e3bade2d2c9c6516bffbe09eb3b85a9f99160
Alfonso Ricaldoni <AR287195@fi365.ort.edu.uy>
2025-10-05 18:38:08 -0300

    RED: createUserResponse id getter

---
+++ tests/VirtualPark.WebApi.Test/Controllers/Users/ModelsOut/CreateUserResponseTest.cs
@@ -0,0 +1,16 @@
+namespace VirtualPark.WebApi.Test.Controllers.Users.ModelsOut;
+
+[TestClass]
+[TestCategory("ModelsOut")]
+[TestCategory("CreateUserResponse")]
+public class CreateUserResponseTest
+{
+    [TestMethod]
+    [TestCategory("Validation")]
+    public void Id_Getter_ReturnsAssignedValue()
+    {
+        var id = Guid.NewGuid().ToString();
+        var response = new CreateUserResponse(id);
+        response.Id.Should().Be(id);
+    }
+}
