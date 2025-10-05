d296008577bb7e7ffcb7f406bdbee7b1e1115c9b
Alfonso Ricaldoni <AR287195@fi365.ort.edu.uy>
2025-10-05 18:40:17 -0300

    REFACTOR: craeteUserResponse id getter

--- tests/VirtualPark.WebApi.Test/Controllers/Users/ModelsOut/CreateUserResponseTest.cs
+++ tests/VirtualPark.WebApi.Test/Controllers/Users/ModelsOut/CreateUserResponseTest.cs
@@ -8,12 +8,14 @@ namespace VirtualPark.WebApi.Test.Controllers.Users.ModelsOut;
 [TestCategory("CreateUserResponse")]
 public class CreateUserResponseTest
 {
+    #region Id
     [TestMethod]
     [TestCategory("Validation")]
-    public void Id_Getter()
+    public void Id_Getter_ReturnsAssignedValue()
     {
         var id = Guid.NewGuid().ToString();
         var response = new CreateUserResponse(id);
         response.Id.Should().Be(id);
     }
+    #endregion
 }
