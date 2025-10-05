4ab336e0d312ce0581b584043d9e2a062ac21574
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-10-05 19:47:28 -0300

    create ticket configuration

--- src/VirtualPark.DataAccess/SqlContext.cs
+++ src/VirtualPark.DataAccess/SqlContext.cs
@@ -34,38 +34,5 @@ public class SqlContext(DbContextOptions<SqlContext> options) : DbContext(option
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
-
-        modelBuilder.Entity<Ranking>(entity =>
-        {
-            
-        });
-
-        modelBuilder.Entity<Ticket>(entity =>
-        {
-            entity.ToTable("Tickets");
-
-            entity.HasKey(t => t.Id);
-            entity.Property(t => t.Id).ValueGeneratedNever();
-
-            entity.Property(t => t.Date).HasColumnType("datetime2");
-            entity.Property(t => t.Type).IsRequired();
-            entity.Property(t => t.QrId).IsRequired();
-
-            entity
-                .HasOne(t => t.Event)
-                .WithMany()
-                .HasForeignKey(t => t.EventId)
-                .OnDelete(DeleteBehavior.Restrict);
-
-            entity
-                .HasOne(t => t.Visitor)
-                .WithMany()
-                .HasForeignKey(t => t.VisitorProfileId)
-                .OnDelete(DeleteBehavior.Restrict);
-
-            entity.HasIndex(t => t.EventId);
-            entity.HasIndex(t => t.VisitorProfileId);
-            entity.HasIndex(t => t.QrId).IsUnique();
-        });
     }
 }
