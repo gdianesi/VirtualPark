2aec3fb4c206990b76222a3666cad9596071bbac
Alfonso Ricaldoni <AR287195@fi365.ort.edu.uy>
2025-10-05 16:24:13 -0300

    REFACTOR: createUserRequest name Getter

--- tests/VirtualPark.WebApi.Test/Controllers/Users/ModelsIn/CreateUserRequestTest.cs
+++ tests/VirtualPark.WebApi.Test/Controllers/Users/ModelsIn/CreateUserRequestTest.cs
@@ -8,11 +8,13 @@ namespace VirtualPark.WebApi.Test.Controllers.Users.ModelsIn;
 [TestCategory("CreateUserRequest")]
 public class CreateUserRequestTest
 {
+    #region Name
     [TestMethod]
     [TestCategory("Validation")]
-    public void Name_Getter()
+    public void Name_Getter_ReturnsAssignedValue()
     {
         var createUserRequest = new CreateUserRequest { Name = "Pepe" };
         createUserRequest.Name.Should().Be("Pepe");
     }
+    #endregion
 }
