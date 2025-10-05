c910be9ac6ce006107751e74e5b4fae4354a7cc3
Alfonso Ricaldoni <AR287195@fi365.ort.edu.uy>
2025-10-05 17:41:50 -0300

    GREEN: createVisitorProfileRequest Membership Getter

--- src/VirtualPark.WebApi/Controllers/VisitorsProfile/ModelsIn/CreateVisitorProfileRequest.cs
+++ src/VirtualPark.WebApi/Controllers/VisitorsProfile/ModelsIn/CreateVisitorProfileRequest.cs
@@ -3,4 +3,5 @@ namespace VirtualPark.WebApi.Controllers.VisitorsProfile.ModelsIn;
 public class CreateVisitorProfileRequest
 {
     public string? DateOfBirth { get; init; }
+    public string? Membership { get; init; }
 }
