3780555c72e9e196fe6c0ec039a4b41bd255cec6
gdianesi <gd329180@fi365.ort.edu.uy>
2025-10-06 09:51:56 -0300

    REFACTOR: cost getter should return assigned value

--- src/VirtualPark.WebApi/Controllers/Events/ModelsOut/GetEventResponse.cs
+++ src/VirtualPark.WebApi/Controllers/Events/ModelsOut/GetEventResponse.cs
@@ -3,8 +3,8 @@ namespace VirtualPark.WebApi.Controllers.Events.ModelsOut;
 public sealed class GetEventResponse(string id, string name, string date, string capacity, string cost)
 {
     public string Capacity { get; } = capacity;
+    public string Cost { get; } = cost;
     public string Date { get; set; } = date;
     public string Id { get; } = id;
     public string Name { get; } = name;
-    public string Cost { get; } = cost;
 }
