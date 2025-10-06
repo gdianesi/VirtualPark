16672677edf031b5f6869341b03a5d9667f0f64e
Alfonso Ricaldoni <AR287195@fi365.ort.edu.uy>
2025-10-06 20:20:33 -0300

    RGR: GetVisitorProfileResponse nfcId

--- tests/VirtualPark.WebApi.Test/Controllers/VisitorsProfile/ModelsOut/GetVisitorProfileResponseTest.cs
+++ tests/VirtualPark.WebApi.Test/Controllers/VisitorsProfile/ModelsOut/GetVisitorProfileResponseTest.cs
@@ -15,7 +15,7 @@ public class GetVisitorProfileResponseTest
     {
         var id = Guid.NewGuid().ToString();
         var response = new GetVisitorProfileResponse(
-            id, "2002-07-30", "Visitor", "10");
+            id, "2002-07-30", "Visitor", "10", Guid.NewGuid().ToString());
         response.Id.Should().Be(id);
     }
     #endregion
@@ -27,7 +27,7 @@ public class GetVisitorProfileResponseTest
     {
         var id = Guid.NewGuid().ToString();
         var response = new GetVisitorProfileResponse(
-            id, "2002-07-30", "Visitor", "10");
+            id, "2002-07-30", "Visitor", "10", Guid.NewGuid().ToString());
         response.DateOfBirth.Should().Be("2002-07-30");
     }
     #endregion
@@ -39,7 +39,7 @@ public class GetVisitorProfileResponseTest
     {
         var id = Guid.NewGuid().ToString();
         var response = new GetVisitorProfileResponse(
-            id, "2002-07-30", "Visitor", "10");
+            id, "2002-07-30", "Visitor", "10", Guid.NewGuid().ToString());
         response.Membership.Should().Be("Visitor");
     }
     #endregion
@@ -51,8 +51,21 @@ public class GetVisitorProfileResponseTest
     {
         var id = Guid.NewGuid().ToString();
         var response = new GetVisitorProfileResponse(
-            id, "2002-07-30", "Visitor", "10");
+            id, "2002-07-30", "Visitor", "10", Guid.NewGuid().ToString());
         response.Score.Should().Be("10");
+    }
+    #endregion
+
+    #region NfcId
+    [TestMethod]
+    [TestCategory("Validation")]
+    public void NfcId_Getter_ReturnsAssignedValue()
+    {
+        var id = Guid.NewGuid().ToString();
+        var nfcId = Guid.NewGuid().ToString();
+        var response = new GetVisitorProfileResponse(
+            id, "2002-07-30", "Visitor", "10", nfcId);
+        response.NfcId.Should().Be(nfcId);
     }
     #endregion
 }
