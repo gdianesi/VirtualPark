36f9d5ef77a8bfe490d638183e23602783319d1b
gdianesi <gd329180@fi365.ort.edu.uy>
2025-10-06 17:41:34 -0300

    REFACTOR: eventId ini modelsIn

--- src/VirtualPark.WebApi/Controllers/Tickets/ModelsIn/CreateTicketRequest.cs
+++ src/VirtualPark.WebApi/Controllers/Tickets/ModelsIn/CreateTicketRequest.cs
@@ -2,7 +2,7 @@ namespace VirtualPark.WebApi.Controllers.Tickets.ModelsIn;
 
 public sealed class CreateTicketRequest(string visitorId, string type, string? eventId)
 {
+    public string? EventId { get; set; } = eventId;
     public string VisitorId { get; set; } = visitorId;
     public string Type { get; set; } = type;
-    public string? EventId { get; set; } = eventId;
 }
