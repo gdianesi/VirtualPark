69dad8057e3b4cf0c3d151f5eb7cf9d2e66ee496
gdianesi <gd329180@fi365.ort.edu.uy>
2025-10-06 20:12:02 -0300

    REFACTOR: CreateTicketResponse

--- src/VirtualPark.WebApi/Controllers/Tickets/ModelsOut/CreateTicketsResponse.cs
+++ src/VirtualPark.WebApi/Controllers/Tickets/ModelsOut/CreateTicketsResponse.cs
@@ -1,11 +1,6 @@
 namespace VirtualPark.WebApi.Controllers.Tickets.ModelsOut;
 
-public sealed class CreateTicketResponse
+public sealed class CreateTicketResponse(string id)
 {
-    public CreateTicketResponse(string id)
-    {
-        Id = id;
-    }
-
-    public string Id { get; init; }
+    public string Id { get; } = id;
 }
