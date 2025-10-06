9341ad5e23cf2ad8e3d0bac3e3582b14c7f7ef5f
Alfonso Ricaldoni <AR287195@fi365.ort.edu.uy>
2025-10-05 21:11:12 -0300

    RED: GetUserResponse visitorProfileId getter

--- tests/VirtualPark.WebApi.Test/Controllers/Users/ModelsOut/GetUserResponseTest.cs
+++ tests/VirtualPark.WebApi.Test/Controllers/Users/ModelsOut/GetUserResponseTest.cs
@@ -9,7 +9,6 @@ namespace VirtualPark.WebApi.Test.Controllers.Users.ModelsOut;
 public class GetUserResponseTest
 {
     #region Id
-
     [TestMethod]
     [TestCategory("Validation")]
     public void Id_Getter_ReturnsAssignedValue()
@@ -23,11 +22,9 @@ public class GetUserResponseTest
             new List<string> { Guid.NewGuid().ToString() });
         response.Id.Should().Be(id);
     }
-
     #endregion
 
     #region Name
-
     [TestMethod]
     [TestCategory("Validation")]
     public void Name_Getter_ReturnsAssignedValue()
@@ -40,11 +37,9 @@ public class GetUserResponseTest
             new List<string> { Guid.NewGuid().ToString() });
         response.Name.Should().Be("pepe");
     }
-
     #endregion
 
     #region LastName
-
     [TestMethod]
     [TestCategory("Validation")]
     public void LastName_Getter_ReturnsAssignedValue()
@@ -57,11 +52,9 @@ public class GetUserResponseTest
             new List<string> { Guid.NewGuid().ToString() });
         response.LastName.Should().Be("perez");
     }
-
     #endregion
 
     #region Email
-
     [TestMethod]
     [TestCategory("Validation")]
     public void Email_Getter_ReturnsAssignedValue()
@@ -74,11 +67,9 @@ public class GetUserResponseTest
             new List<string> { Guid.NewGuid().ToString() });
         response.Email.Should().Be("pepe@gmail.com");
     }
-
     #endregion
 
     #region Roles
-
     [TestMethod]
     [TestCategory("Validation")]
     public void Role_Getter_ReturnsAssignedValue()
@@ -92,6 +83,20 @@ public class GetUserResponseTest
             new List<string> { guid });
         response.Roles.Should().Contain([guid]);
     }
-
     #endregion
+
+    [TestMethod]
+    [TestCategory("Validation")]
+    public void VisitorProfile_Getter()
+    {
+        var id = Guid.NewGuid().ToString();
+        var guid = Guid.NewGuid().ToString();
+        var response = new GetUserResponse(id,
+            "pepe",
+            "perez",
+            "pepe@gmail.com",
+            new List<string> { Guid.NewGuid().ToString() },
+            guid);
+        response.VisitorProfileId.Should().Be(guid);
+    }
 }
