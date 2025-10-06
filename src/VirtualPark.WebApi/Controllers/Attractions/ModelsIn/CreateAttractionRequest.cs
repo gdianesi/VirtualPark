e13f4657edd877c64d457fe944cb9c47357af3a3
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-10-06 00:20:23 -0300

    REFACTOR: CreateAttractionRequest_TypeIdProperty_GetAndSet_ShouldWorkCorrectly

--- src/VirtualPark.WebApi/Controllers/Attractions/ModelsIn/CreateAttractionRequest.cs
+++ src/VirtualPark.WebApi/Controllers/Attractions/ModelsIn/CreateAttractionRequest.cs
@@ -3,5 +3,5 @@ namespace VirtualPark.WebApi.Controllers.Attractions.ModelsIn;
 public sealed class CreateAttractionRequest
 {
     public string? Name { get; init; }
-    public string? TypeId { get; set; }
+    public string? TypeId { get; init; }
 }
