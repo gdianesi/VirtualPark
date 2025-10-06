2d4300aad135595ec4b06ba936c0e35cc1dd11de
gdianesi <gd329180@fi365.ort.edu.uy>
2025-10-06 09:02:50 -0300

    REFACTOR: createEventRequest should return cost value assigned

--- src/VirtualPark.WebApi/Controllers/Events/ModelsIn/CreateEventRequest.cs
+++ src/VirtualPark.WebApi/Controllers/Events/ModelsIn/CreateEventRequest.cs
@@ -3,7 +3,7 @@ namespace VirtualPark.WebApi.Controllers.Events.ModelsIn;
 public sealed class CreateEventRequest
 {
     public string? Capacity { get; set; }
+    public string? Cost { get; set; }
     public string? Date { get; init; }
     public string? Name { get; init; }
-    public string? Cost { get; set; }
 }
