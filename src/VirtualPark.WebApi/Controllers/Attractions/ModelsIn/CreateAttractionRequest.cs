7623f4e4fdc608572e2f789d1095c7cdc5b860ee
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-10-06 01:29:40 -0300

    GREEN: to args

--- src/VirtualPark.WebApi/Controllers/Attractions/ModelsIn/CreateAttractionRequest.cs
+++ src/VirtualPark.WebApi/Controllers/Attractions/ModelsIn/CreateAttractionRequest.cs
@@ -11,4 +11,16 @@ public sealed class CreateAttractionRequest
     public string? Capacity { get; init; }
     public string? Description { get; init; }
     public string? Available { get; init; }
+
+    public AttractionArgs ToArgs()
+    {
+        return new AttractionArgs(
+            ValidationServices.ValidateNullOrEmpty(Type),
+            ValidationServices.ValidateNullOrEmpty(Name),
+            ValidationServices.ValidateNullOrEmpty(MiniumAge),
+            ValidationServices.ValidateNullOrEmpty(Capacity),
+            ValidationServices.ValidateNullOrEmpty(Description),
+            "0",
+            ValidationServices.ValidateNullOrEmpty(Available));
+    }
 }
