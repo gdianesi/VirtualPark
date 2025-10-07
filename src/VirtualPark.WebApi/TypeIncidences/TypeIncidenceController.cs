3bf9fa18bc43cc327c0b8ca6401b2de597f21aa0
Alfonso Ricaldoni <AR287195@fi365.ort.edu.uy>
2025-10-06 22:54:44 -0300

    GR: TypeIncidenceController Create

--- src/VirtualPark.WebApi/TypeIncidences/TypeIncidenceController.cs
+++ src/VirtualPark.WebApi/TypeIncidences/TypeIncidenceController.cs
@@ -7,6 +7,17 @@ using VirtualPark.WebApi.TypeIncidences.ModelsOut;
 namespace VirtualPark.WebApi.TypeIncidences;
 
 [ApiController]
-public sealed class TypeIncidenceController() : ControllerBase
+public sealed class TypeIncidenceController(ITypeIncidenceService service) : ControllerBase
 {
+    private readonly ITypeIncidenceService _service = service;
+
+    [HttpPost("typeIncidences")]
+    public CreateTypeIncidenceResponse CreateTypeIncidence(CreateTypeIncidenceRequest request)
+    {
+        var args = request.ToArgs();
+
+        Guid id = _service.Create(args);
+
+        return new CreateTypeIncidenceResponse(id.ToString());
+    }
 }
