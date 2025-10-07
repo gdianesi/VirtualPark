e9e1286b9f13954256a219b0d58f29467a54408b
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-10-06 23:52:05 -0300

    GREN: Id_Getter_ReturnsAssignedValue

--- src/VirtualPark.WebApi/Controllers/Roles/ModelsOut/GetRoleResponse.cs
+++ src/VirtualPark.WebApi/Controllers/Roles/ModelsOut/GetRoleResponse.cs
@@ -2,5 +2,5 @@ namespace VirtualPark.WebApi.Controllers.Roles.ModelsOut;
 
 public class GetRoleResponse(string id, string name, string description, List<string> permissionIds, List<string> usersIds)
 {
-    
+    public string Id { get; init; } = id;
 }
