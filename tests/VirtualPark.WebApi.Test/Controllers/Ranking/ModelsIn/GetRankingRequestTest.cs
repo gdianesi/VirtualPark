1d5b0a9409e9409b3a2591541ebc9f61fa859547
gdianesi <gd329180@fi365.ort.edu.uy>
2025-10-07 00:21:06 -0300

    RED: getRanking date

---
+++ tests/VirtualPark.WebApi.Test/Controllers/Ranking/ModelsIn/GetRankingRequestTest.cs
@@ -0,0 +1,16 @@
+namespace VirtualPark.WebApi.Test.Controllers.Ranking.ModelsIn;
+
+[TestClass]
+[TestCategory("ModelsIn")]
+[TestCategory("GetRankingRequest")]
+public sealed class GetRankingRequestTest
+{
+    #region Date
+    [TestMethod]
+    [TestCategory("Validation")]
+    public void GetRankingRequest_DateProperty_ShouldReturnAssignedValue()
+    {
+        var request = new GetRankingRequest { Date = "2025-10-06" };
+        request.Date.Should().Be("2025-10-06");
+    }
+#endregion
