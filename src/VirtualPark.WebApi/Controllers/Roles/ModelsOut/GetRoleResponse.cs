af565e0c9d907d7d4e28aee2f4da6bdd50af49a0
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-10-06 23:53:07 -0300

    GREEN: Name_Getter_ReturnsAssignedValue

--- src/VirtualPark.WebApi/Controllers/Roles/ModelsOut/GetRoleResponse.cs
+++ src/VirtualPark.WebApi/Controllers/Roles/ModelsOut/GetRoleResponse.cs
@@ -3,4 +3,5 @@ namespace VirtualPark.WebApi.Controllers.Roles.ModelsOut;
 public class GetRoleResponse(string id, string name, string description, List<string> permissionIds, List<string> usersIds)
 {
     public string Id { get; init; } = id;
+    public string Name { get; init; } = name;
 }
