4646132bc31e46eb45e097b0925eddb8c87add89
Alfonso Ricaldoni <AR287195@fi365.ort.edu.uy>
2025-10-06 22:14:40 -0300

    RED: CreateTypeIncidenceResponse id

---
+++ tests/VirtualPark.WebApi.Test/Controllers/VisitorsProfile/ModelsOut/CreateTypeIncidenceResponseTest.cs
@@ -0,0 +1,18 @@
+namespace VirtualPark.WebApi.Test.Controllers.VisitorsProfile.ModelsOut;
+
+[TestClass]
+[TestCategory("ModelsOut")]
+[TestCategory("CreateTypeIncidenceResponse")]
+public class CreateTypeIncidenceResponseTest
+{
+    #region Id
+    [TestMethod]
+    [TestCategory("Validation")]
+    public void Id_Getter_ReturnsAssignedValue()
+    {
+        var id = Guid.NewGuid().ToString();
+        var response = new CreateTypeIncidenceResponse(id);
+        response.Id.Should().Be(id);
+    }
+    #endregion
+}
