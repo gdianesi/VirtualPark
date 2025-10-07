a515c271687a4301ead24a12f6bab07ea9d37246
Alfonso Ricaldoni <AR287195@fi365.ort.edu.uy>
2025-10-07 02:43:47 -0300

    GREEN: GetUserLoggedSessionResponse id

--- src/VirtualPark.WebApi/Controllers/Sessions/ModelsOut/GetUserLoggedSessionResponse.cs
+++ src/VirtualPark.WebApi/Controllers/Sessions/ModelsOut/GetUserLoggedSessionResponse.cs
@@ -1,5 +1,6 @@
 namespace VirtualPark.WebApi.Controllers.Sessions.ModelsOut;
 
-public class GetUserLoggedSessionResponse
+public class GetUserLoggedSessionResponse(string id)
 {
+    public string Id { get; } = id;
 }
--- tests/VirtualPark.WebApi.Test/Controllers/Sessions/ModelsOut/GetUserLoggedSessionResponseTest.cs
+++ tests/VirtualPark.WebApi.Test/Controllers/Sessions/ModelsOut/GetUserLoggedSessionResponseTest.cs
@@ -1,3 +1,4 @@
+using FluentAssertions;
 using VirtualPark.WebApi.Controllers.Sessions.ModelsOut;
 
 namespace VirtualPark.WebApi.Test.Controllers.Sessions.ModelsOut;
