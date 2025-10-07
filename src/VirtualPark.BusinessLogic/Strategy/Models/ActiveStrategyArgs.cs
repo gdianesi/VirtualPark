e996becce316943ef700282e7100f4cd125108e4
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-10-07 20:04:22 -0300

    RED: strategy args

---
+++ src/VirtualPark.BusinessLogic/Strategy/Models/ActiveStrategyArgs.cs
@@ -0,0 +1,7 @@
+using VirtualPark.BusinessLogic.Validations.Services;
+
+namespace VirtualPark.BusinessLogic.Strategy.Models;
+
+public class ActiveStrategyArgs(string strategyKey, string date)
+{
+}
--- src/VirtualPark.BusinessLogic/Strategy/Services/StrategyService.cs
+++ src/VirtualPark.BusinessLogic/Strategy/Services/StrategyService.cs
@@ -1,7 +1,11 @@
-using VirtualPark.BusinessLogic.VisitRegistrations.Entity;
+using VirtualPark.BusinessLogic.Strategy.Entity;
+using VirtualPark.BusinessLogic.Strategy.Models;
+using VirtualPark.Repository;
 
 namespace VirtualPark.BusinessLogic.Strategy.Services;
 
-public sealed class StrategyService
+public sealed class ActiveStrategyService(IRepository<ActiveStrategy> activeStrategyRepository )
 {
+    private readonly IRepository<ActiveStrategy> _activeStrategyRepository = activeStrategyRepository;
+    
 }
