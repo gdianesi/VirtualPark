0730c4089e8bfa698c3f731e0399e10f44676bc0
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-10-07 22:10:13 -0300

    green GetActiveStrategyResponse

--- src/VirtualPark.WebApi/Controllers/Strategy/ModelsOut/GetActiveStrategyResponse.cs
+++ src/VirtualPark.WebApi/Controllers/Strategy/ModelsOut/GetActiveStrategyResponse.cs
@@ -2,4 +2,7 @@ namespace VirtualPark.WebApi.Controllers.Strategy.ModelsOut;
 
 public class GetActiveStrategyResponse(string id, string key, string date)
 {
+    public string Id { get; } = id;
+    public string Key { get; } = key;
+    public string Date { get; } = date;
 }
