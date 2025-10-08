b26792bf4a6cbaf1ed0cd26bd504075508359664
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-10-07 22:57:42 -0300

    GREEn strategy controller

--- src/VirtualPark.WebApi/Controllers/Strategy/StrategyController.cs
+++ src/VirtualPark.WebApi/Controllers/Strategy/StrategyController.cs
@@ -13,5 +13,54 @@ public class StrategyController(IStrategyService strategyService) : ControllerBa
 {
     private readonly IStrategyService _strategyService = strategyService;
 
-   
+    [HttpPost]
+    public CreateActiveStrategyResponse CreateActiveStrategy(CreateActiveStrategyRequest request)
+    {
+        ActiveStrategyArgs strategyArgs = request.ToArgs();
+
+        Guid strategyId = _strategyService.Create(strategyArgs);
+
+        return new CreateActiveStrategyResponse(strategyId.ToString());
+    }
+
+    [HttpGet("{date}")]
+    public ActionResult<GetActiveStrategyResponse> GetActiveStrategy(string date)
+    {
+        var dateOnly = ValidationServices.ValidateDateOnly(date);
+
+        var strategy = _strategyService.Get(dateOnly);
+
+        return new GetActiveStrategyResponse(
+            key: strategy.StrategyKey,
+            date: strategy.Date.ToString("yyyy-MM-dd"));
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<GetActiveStrategyResponse>> GetActiveStrategies()
+    {
+        var list = _strategyService.GetAll()
+            .Select(a => new GetActiveStrategyResponse(
+                key: a.StrategyKey,
+                date: a.Date.ToString("yyyy-MM-dd")))
+            .ToList();
+
+        return list;
+    }
+
+    [HttpDelete("{date}")]
+    public IActionResult DeleteStrategy(string date)
+    {
+        var dateOnly = ValidationServices.ValidateDateOnly(date);
+        _strategyService.Remove(dateOnly);
+        return NoContent();
+    }
+
+    [HttpPut("{date}")]
+    public IActionResult UpdateStrategy([FromBody] UpdateActiveStrategyRequest request, string date)
+    {
+        var dateOnly = ValidationServices.ValidateDateOnly(date);
+        var args = request.ToArgs(dateOnly);
+        _strategyService.Update(args, dateOnly);
+        return NoContent();
+    }
 }
