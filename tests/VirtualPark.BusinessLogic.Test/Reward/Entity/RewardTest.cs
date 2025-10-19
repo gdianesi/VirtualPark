18683299a8ee30e7f43b7f2f13b8be1a1d04400d
gdianesi <gd329180@fi365.ort.edu.uy>
2025-10-19 15:36:30 -0300

    RED: reward description

--- tests/VirtualPark.BusinessLogic.Test/Reward/Entity/RewardTest.cs
+++ tests/VirtualPark.BusinessLogic.Test/Reward/Entity/RewardTest.cs
@@ -4,9 +4,9 @@ namespace VirtualPark.BusinessLogic.Test.Reward.Entity;
 
 using VirtualPark.BusinessLogic.Rewards.Entity;
 
-#region Name
 [TestClass]
 [TestCategory("Entity")]
+#region Name
 public sealed class RewardTest
 {
     [TestMethod]
@@ -19,5 +19,17 @@ public sealed class RewardTest
 
         reward.Name.Should().Be(name);
     }
+    #endregion
+    [TestMethod]
+    [TestCategory("Success")]
+    public void Constructor_WhenDescriptionIsValid_ShouldSetDescription()
+    {
+        const string name = "Entrada VIP";
+        const string description = "Acceso exclusivo a todas las atracciones sin filas";
+
+        var reward = new Reward { Name = name, Description = description };
+
+        reward.Description.Should().Be(description);
+    }
 }
-#endregion
+
