7b39d278eeb3fe8ef4638620fe4f5c8410e653b1
gdianesi <gd329180@fi365.ort.edu.uy>
2025-10-20 16:51:29 -0300

    GREEN: getRewardRespone name

--- src/VirtualPark.WebApi/Controllers/Reward/ModelsOut/GetRewardResponse.cs
+++ src/VirtualPark.WebApi/Controllers/Reward/ModelsOut/GetRewardResponse.cs
@@ -1,6 +1,7 @@
 namespace VirtualPark.WebApi.Controllers.Reward.ModelsOut;
 
-public class GetRewardResponse(string id)
+public class GetRewardResponse(string id, string name)
 {
     public string Id { get; } = id;
+    public object Name { get; } = name;
 }
--- tests/VirtualPark.WebApi.Test/Controllers/Reward/ModelsOut/GetRewardResponseTest.cs
+++ tests/VirtualPark.WebApi.Test/Controllers/Reward/ModelsOut/GetRewardResponseTest.cs
@@ -8,13 +8,24 @@ namespace VirtualPark.WebApi.Test.Controllers.Reward.ModelsOut;
 [TestCategory("GetRewardResponse")]
 public sealed class GetRewardResponseTest
 {
+    #region Id
     [TestMethod]
     [TestCategory("Validation")]
     public void Id_Getter_ReturnsAssignedValue()
     {
         var id = Guid.NewGuid().ToString();
-        var response = new GetRewardResponse(id);
+        var response = new GetRewardResponse(id, "VIP Ticket");
 
         response.Id.Should().Be(id);
+    }
+    #endregion
+
+    [TestMethod]
+    [TestCategory("Validation")]
+    public void Name_Getter_ReturnsAssignedValue()
+    {
+        var response = new GetRewardResponse(Guid.NewGuid().ToString(), "VIP Ticket");
+
+        response.Name.Should().Be("VIP Ticket");
     }
 }
