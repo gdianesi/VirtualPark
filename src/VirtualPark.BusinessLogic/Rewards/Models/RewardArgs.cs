450310efe8cd957953a1c3ead36290f30f887579
gdianesi <gd329180@fi365.ort.edu.uy>
2025-10-19 22:27:28 -0300

    GREEN: invalid name in rewardArgs

--- src/VirtualPark.BusinessLogic/Rewards/Models/RewardArgs.cs
+++ src/VirtualPark.BusinessLogic/Rewards/Models/RewardArgs.cs
@@ -1,6 +1,8 @@
+using VirtualPark.BusinessLogic.Validations.Services;
+
 namespace VirtualPark.BusinessLogic.Rewards.Models;
 
 public sealed class RewardArgs(string name)
 {
-    public string Name { get; init; } = name;
+    public string Name { get; } = ValidationServices.ValidateNullOrEmpty(name);
 }
