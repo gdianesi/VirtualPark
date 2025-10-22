78fdaedfb3f487a0e160321c51d881c28071d2f9
Alfonso Ricaldoni <AR287195@fi365.ort.edu.uy>
2025-10-22 13:17:55 -0300

    GREEN: visitScore points should be setter

--- src/VirtualPark.BusinessLogic/VisitsScore/Entity/VisitScore.cs
+++ src/VirtualPark.BusinessLogic/VisitsScore/Entity/VisitScore.cs
@@ -5,5 +5,5 @@ public class VisitScore
     public Guid Id { get; init; } = Guid.NewGuid();
     public string Origin { get; set; } = null!;
     public DateTime OccurredAt { get; set; }
-    public int Points { get; init; }
+    public int Points { get; set; }
 }
