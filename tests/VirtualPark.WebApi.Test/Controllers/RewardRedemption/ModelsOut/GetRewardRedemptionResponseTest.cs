fd869b617c3556a0eb6fafdaf8ee4e2bf2b24280
gdianesi <gd329180@fi365.ort.edu.uy>
2025-10-21 21:20:47 -0300

    RED: Id in get reward redemption request

---
+++ tests/VirtualPark.WebApi.Test/Controllers/RewardRedemption/ModelsOut/GetRewardRedemptionResponseTest.cs
@@ -0,0 +1,19 @@
+namespace VirtualPark.WebApi.Test.Controllers.RewardRedemption.ModelsOut;
+
+[TestClass]
+[TestCategory("ModelsOut")]
+[TestCategory("GetRewardRedemptionResponse")]
+public sealed class GetRewardRedemptionResponseTest
+{
+    [TestMethod]
+    [TestCategory("Validation")]
+    public void Id_Getter_ReturnsAssignedValue()
+    {
+        var id = Guid.NewGuid().ToString();
+
+        var response = new GetRewardRedemptionResponse(
+            id);
+
+        response.Id.Should().Be(id);
+    }
+}
