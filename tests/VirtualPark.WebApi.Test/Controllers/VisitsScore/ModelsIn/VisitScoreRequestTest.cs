40760738b141e1e7084bae08d38672c01df0833d
Alfonso Ricaldoni <AR287195@fi365.ort.edu.uy>
2025-10-24 14:06:03 -0300

    RED:VisitScoreRequest toArgs

--- tests/VirtualPark.WebApi.Test/Controllers/VisitsScore/ModelsIn/VisitScoreRequestTest.cs
+++ tests/VirtualPark.WebApi.Test/Controllers/VisitsScore/ModelsIn/VisitScoreRequestTest.cs
@@ -38,4 +38,44 @@ public class VisitScoreRequestTest
         req.Points.Should().Be("-30");
     }
     #endregion
+
+    #region ToArgs
+    [TestMethod]
+    [TestCategory("ToArgs")]
+    public void ToArgs_ShouldMapAndTrimOrigin_AndAllowNullPoints()
+    {
+        var vrId = Guid.NewGuid();
+        var req = new VisitScoreRequest
+        {
+            VisitRegistrationId = vrId.ToString(),
+            Origin = "  Atracción  ",
+            Points = null
+        };
+
+        var args = req.ToArgs();
+
+        args.VisitRegistrationId.Should().Be(vrId);
+        args.Origin.Should().Be("Atracción");
+        args.Points.Should().BeNull();
+    }
+
+    [TestMethod]
+    [TestCategory("ToArgs")]
+    public void ToArgs_ShouldParsePoints_WhenProvidedAsIntegerString()
+    {
+        var vrId = Guid.NewGuid();
+        var req = new VisitScoreRequest
+        {
+            VisitRegistrationId = vrId.ToString(),
+            Origin = "Canje",
+            Points = "-50"
+        };
+
+        var args = req.ToArgs();
+
+        args.VisitRegistrationId.Should().Be(vrId);
+        args.Origin.Should().Be("Canje");
+        args.Points.Should().Be(-50);
+    }
+    #endregion
 }
