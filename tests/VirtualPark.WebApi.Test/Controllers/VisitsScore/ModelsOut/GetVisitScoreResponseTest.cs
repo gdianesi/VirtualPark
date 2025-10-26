9057d0988b35120368376c6f44a3e9a1e3efe229
Alfonso Ricaldoni <AR287195@fi365.ort.edu.uy>
2025-10-25 21:19:33 -0300

    RED: GetVisiScoreResponse id

---
+++ tests/VirtualPark.WebApi.Test/Controllers/VisitsScore/ModelsOut/GetVisitScoreResponseTest.cs
@@ -0,0 +1,22 @@
+namespace VirtualPark.WebApi.Test.Controllers.VisitsScore.ModelsOut;
+
+[TestClass]
+[TestCategory("ModelsOut")]
+[TestCategory("GetVisitScoreResponse")]
+public class GetVisitScoreResponseTest
+{
+    #region Id
+    [TestMethod]
+    [TestCategory("Validation")]
+    public void Id_Getter_ReturnsAssignedValue()
+    {
+        var id = Guid.NewGuid().ToString();
+        var regId = Guid.NewGuid().ToString();
+
+        var resp = new GetVisitScoreResponse(
+            id);
+
+        resp.Id.Should().Be(id);
+    }
+    #endregion
+}
