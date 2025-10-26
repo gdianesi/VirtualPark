47d11e483358e2d24f76cac9f010592f081aa3f4
Alfonso Ricaldoni <AR287195@fi365.ort.edu.uy>
2025-10-25 21:27:39 -0300

    GREEN: GetVisiScoreResponse origin

--- src/VirtualPark.WebApi/Controllers/VisitsScore/ModelsOut/GetVisitScoreResponse.cs
+++ src/VirtualPark.WebApi/Controllers/VisitsScore/ModelsOut/GetVisitScoreResponse.cs
@@ -1,6 +1,7 @@
 namespace VirtualPark.WebApi.Controllers.VisitsScore.ModelsOut;
 
-public class GetVisitScoreResponse(string id)
+public class GetVisitScoreResponse(string id, string origin)
 {
     public string Id { get; } = id;
+    public string Origin { get; } = origin;
 }
