d3c08b6e6255c06e89c98201cdd20a6125e7cf42
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-10-26 12:30:47 -0300

    refactor app routing module

---
+++ VirtualParkUI/src/app/user/user-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { UserRegisterPageComponent } from './user-register-page/user-register-page.component';
+
+const routes: Routes = [
+    { path: '/register', component: UserRegisterPageComponent }
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+export class UserRoutingModule {}
