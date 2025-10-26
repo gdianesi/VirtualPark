7b7b57371e049095d3265bb72819a1cce141b2ff
Alfonso Ricaldoni <AR287195@fi365.ort.edu.uy>
2025-10-25 21:12:14 -0300

    GREEN: GetScoresByVisitorId

--- src/VirtualPark.BusinessLogic/VisitsScore/Service/VisitScoreService.cs
+++ src/VirtualPark.BusinessLogic/VisitsScore/Service/VisitScoreService.cs
@@ -3,6 +3,19 @@ using VirtualPark.DataAccess;
 
 namespace VirtualPark.BusinessLogic.VisitsScore.Service;
 
-public class VisitScoreService
+public class VisitScoreService(
+    IVisitScoreRepository visitScoreRepository
+) : IVisitScoreService
 {
+    private readonly IVisitScoreRepository _visitScoreRepository = visitScoreRepository;
+
+    public List<VisitScore> GetScoresByVisitorId(Guid visitorId)
+    {
+        if (visitorId == Guid.Empty)
+        {
+            throw new ArgumentException("Visitor ID no puede ser vacío.");
+        }
+
+        return _visitScoreRepository.ListByVisitorId(visitorId);
+    }
 }
--- tests/VirtualPark.BusinessLogic.Test/VisitsScore/Service/VisitScoreServiceTest.cs
+++ tests/VirtualPark.BusinessLogic.Test/VisitsScore/Service/VisitScoreServiceTest.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Collections.Generic;
 using FluentAssertions;
 using Moq;
 using VirtualPark.BusinessLogic.VisitsScore.Entity;
