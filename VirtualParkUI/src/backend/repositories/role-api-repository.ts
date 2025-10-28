c951f33caddf28677a670697c19b3441c7c409fc
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-10-27 22:29:26 -0300

    create role service

--- VirtualParkUI/src/backend/repositories/role-api-repository.ts
+++ VirtualParkUI/src/backend/repositories/role-api-repository.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import GenericRepository from './generic-api-repository'
+import GenericApiRepository from './generic-api-repository'
 import { RoleModel } from '../services/role/models/RoleModel'
 import { CreateRoleRequest } from '../services/role/models/CreateRoleRequest';
 import { CreateRoleResponse } from '../services/role/models/CreateRoleResponse';
 
 @Injectable({providedIn: 'root'})
-export class RoleApiRepository extends GenericRepository{
+export class RoleApiRepository extends GenericApiRepository{
     constructor(http: HttpClient) {
         super('roles', http);
     }
---
+++ VirtualParkUI/src/backend/services/role/role.service.ts
@@ -0,0 +1,7 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { RoleModel } from './models/RoleModel'
+import { CreateRoleRequest } from './models/CreateRoleRequest';
+import { CreateRoleResponse } from './models/CreateRoleResponse';
+import { RoleApiRepository } from '../../repositories/role-api-repository';
+
