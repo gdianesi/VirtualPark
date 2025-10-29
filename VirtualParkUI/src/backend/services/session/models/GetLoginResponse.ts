4922cb9782fe611285f2fc447254807d15e22bc5
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-10-28 22:16:51 -0300

    GetLoginResponse

---
+++ VirtualParkUI/src/backend/services/session/models/GetLoginResponse.ts
@@ -0,0 +1,3 @@
+export interface GetLoginResponse {
+    id: string;
+}
--- VirtualParkUI/src/backend/services/session/session.service.ts
+++ VirtualParkUI/src/backend/services/session/session.service.ts
@@ -2,10 +2,21 @@ import { Observable } from 'rxjs';
 import {Injectable} from "@angular/core";
 import { LoginResponse } from './models/LoginResponse'
 import { LoginRequest } from './models/LoginRequest';
+import { SessionApiRepository } from '../../repositories/session-api-repository'
 
 @Injectable({
     providedIn: 'root'
 })
 export class SessionService {
+    constructor(private readonly _sessionRepositoy: SessionApiRepository) {}
     
+    login(credentials: LoginRequest): Observable<LoginResponse>{
+        return this._sessionRepositoy.login(credentials);
+    }
+
+    logout(token: string): Observable<void>{
+        return this._sessionRepositoy.logout(token);
+    }
+    
+    getSession()
 }
