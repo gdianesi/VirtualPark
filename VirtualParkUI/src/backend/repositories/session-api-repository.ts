d33db81d461c3e7fa6ade6aab43803f2a8b26402
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-10-28 22:01:41 -0300

    SessionApiRepository

---
+++ VirtualParkUI/src/backend/repositories/session-api-repository.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import GenericApiRepository from './generic-api-repository';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class SessionApiRepository extends GenericApiRepository {
+    constructor(http: HttpClient) {
+        super('sessions', http);
+    }
+
+    login(credentials: LoginRequest): Observable<LoginResponse> {
+        return this.create<LoginResponse>(credentials, false);
+    }
+
+    logout(token: string): Observable<void> {
+        return this.deleteById<void>(token);
+    }
+
+    getSessionByToken(token: string): Observable<GetSessionResponse> {
+        return this.getById<GetSessionResponse>(token);
+    }
+}
