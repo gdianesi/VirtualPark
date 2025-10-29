c9e1ffc7bf2b0ab448c021d9936c5890fa18e30e
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-10-28 22:47:21 -0300

    create user login component

---
+++ VirtualParkUI/src/app/user/user-login-page/user-login-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-login-page',
+  standalone: false,
+  templateUrl: './user-login-page.component.html',
+  styleUrl: './user-login-page.component.css'
+})
+export class UserLoginPageComponent {
+
+}
--- VirtualParkUI/src/app/user/user.module.ts
+++ VirtualParkUI/src/app/user/user.module.ts
@@ -10,6 +10,6 @@ const routes: Routes = [
 
 @NgModule({
     imports: [RouterModule.forChild(routes)], 
-    exports: [RouterModule]
+    exports: [RouterModule], declarations: [UserLoginPageComponent]
 })
 export class UserModule {}
