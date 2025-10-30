b33baed85f7a15a5f2bddbad552a0931e3575cdc
gdianesi <gd329180@fi365.ort.edu.uy>
2025-10-30 17:25:06 -0300

    RED: add visitorId and visitRegistrationId to getUser

--- tests/VirtualPark.WebApi.Test/Controllers/Sessions/ModelsOut/LogInSessionResponseTest.cs
+++ tests/VirtualPark.WebApi.Test/Controllers/Sessions/ModelsOut/LogInSessionResponseTest.cs
@@ -18,4 +18,21 @@ public class LogInSessionResponseTest
         response.Token.Should().Be(token);
     }
     #endregion
+
+    #region VisitorId
+    [TestMethod]
+    [TestCategory("Validation")]
+    public void Constructor_WithVisitorAndRole_ShouldAssignAllProperties()
+    {
+        var id = Guid.NewGuid().ToString();
+        var visitorId = Guid.NewGuid().ToString();
+        var visitRegistrationId = Guid.NewGuid().ToString();
+
+        var response = new GetUserLoggedSessionResponse(id, visitorId, visitRegistrationId);
+
+        response.Id.Should().Be(id);
+        response.VisitorId.Should().Be(visitorId);
+        response.VisitRegistrationId.Should().Be(role);
+    }
+    #endregion
 }
