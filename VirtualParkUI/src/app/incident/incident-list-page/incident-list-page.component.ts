b0985fc4533afcb04b2816b3522b60fc1508f76b
gdianesi <gd329180@fi365.ort.edu.uy>
2025-11-03 17:09:52 -0300

    add incident-list component

---
+++ VirtualParkUI/src/app/incident/incident-list-page/incident-list-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-incident-list-page',
+  standalone: false,
+  templateUrl: './incident-list-page.component.html',
+  styleUrl: './incident-list-page.component.css'
+})
+export class IncidentListPageComponent {
+
+}
