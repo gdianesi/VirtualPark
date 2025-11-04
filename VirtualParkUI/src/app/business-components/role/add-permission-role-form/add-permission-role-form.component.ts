013a837172675f52c905c857376a052835560066
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-11-03 21:38:10 -0300

    role routing

---
+++ VirtualParkUI/src/app/business-components/role/add-permission-role-form/add-permission-role-form.component.ts
@@ -0,0 +1,28 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+    selector: 'app-add-permission-role-form',
+    templateUrl: './add-permission-role-form.component.html',
+    styleUrls: ['./add-permission-role-form.component.css']
+})
+export class AddPermissionRoleFormComponent {
+    @Output() formSubmit = new EventEmitter<any>();
+
+    form: FormGroup;
+
+    constructor(private fb: FormBuilder) {
+        this.form = this.fb.group({
+            roleId: ['', Validators.required],
+            permissionsIds: [[]],
+        });
+    }
+
+    onSubmit(): void {
+        if (this.form.valid) {
+            this.formSubmit.emit(this.form.value);
+        } else {
+            this.form.markAllAsTouched();
+        }
+    }
+}
