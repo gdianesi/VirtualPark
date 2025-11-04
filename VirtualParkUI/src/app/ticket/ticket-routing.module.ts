5d2ec74956ecfa5dc0f64e2051cd58dc3d76c074
Alfonso Ricaldoni <AR287195@fi365.ort.edu.uy>
2025-11-04 11:21:08 -0300

    add ticket list page

--- VirtualParkUI/src/app/ticket/ticket-routing.module.ts
+++ VirtualParkUI/src/app/ticket/ticket-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TicketRegisterPageComponent } from './ticket-register-page/ticket-register-page.component';
+import { TicketListPageComponent } from './ticket-list-page/ticket-list-page.component';
 
 const routes: Routes = [
-  { path: 'register', component: TicketRegisterPageComponent },
+    { path: '', component: TicketListPageComponent },
+    { path: 'register', component: TicketRegisterPageComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule],
 })
-export class TicketRoutingModule {}
+export class TicketRoutingModule { }
