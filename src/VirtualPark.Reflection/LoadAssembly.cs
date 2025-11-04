b51c5319c3bb19a41e5527baaa81b06d60670fbb
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-11-04 20:02:17 -0300

    Refactor LoadAssembly

--- src/VirtualPark.Reflection/LoadAssembly.cs
+++ src/VirtualPark.Reflection/LoadAssembly.cs
@@ -1,8 +1,17 @@
 namespace VirtualPark.Reflection;
 
-public sealed class LoadAssembly<ITInterface>(string path)
+public sealed class LoadAssembly<ITInterface>(string path) : ILoadAssembly<ITInterface>
     where ITInterface : class
 {
     private readonly DirectoryInfo directory = new(path);
     private List<Type> implementations = [];
+    public List<string> GetImplementations()
+    {
+        throw new NotImplementedException();
+    }
+
+    public ITInterface GetImplementation(string assemblyName, params object[] args)
+    {
+        throw new NotImplementedException();
+    }
 }
