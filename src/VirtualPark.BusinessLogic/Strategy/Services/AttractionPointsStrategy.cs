4339ec5ef52826d85f2a202b46fe85e71930d280
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-11-05 19:07:29 -0300

    Refactor AttractionPointsStrategy

--- src/VirtualPark.BusinessLogic/Strategy/Services/AttractionPointsStrategy.cs
+++ src/VirtualPark.BusinessLogic/Strategy/Services/AttractionPointsStrategy.cs
@@ -1,37 +1,42 @@
 using VirtualPark.BusinessLogic.Attractions;
-using VirtualPark.BusinessLogic.Attractions.Entity;
-using VirtualPark.BusinessLogic.Sessions.Entity;
+
 using VirtualPark.BusinessLogic.Sessions.Service;
+using VirtualPark.BusinessLogic.VisitRegistrations.Entity;
 using VirtualPark.ReflectionAbstractions;
 using VirtualPark.Repository;
 
 namespace VirtualPark.BusinessLogic.Strategy.Services;
 
-public class AttractionPointsStrategy(ISessionService sessionService) : IStrategy
+public class AttractionPointsStrategy(ISessionService sessionService, IReadOnlyRepository<VisitRegistration> visitRegistrationRepository) : IStrategy
 {
     private readonly ISessionService _sessionService = sessionService;
+    private readonly IReadOnlyRepository<VisitRegistration> _visitRegistrationRepository = visitRegistrationRepository;
+
     public string Key { get; } = "Attraction";
 
-    public int CalculatePoints()
+    public int CalculatePoints(Guid token)
     {
-        var visitorProfile = _sessionService.GetUserLogged();
-        if(visit.Attractions.Count == 0)
+        var user = _sessionService.GetUserLogged(token);
+
+        if(user == null)
+        {
+            throw new ApplicationException("No user logged");
+        }
+
+        var visit = _visitRegistrationRepository.Get(v => v.Visitor.Id == user.Id && v.IsActive);
+
+        if (visit == null || visit.Attractions == null || visit.Attractions.Count == 0)
         {
             return 0;
         }
 
-        var totalPoints = 0;
-
-        foreach(var attraction in visit.Attractions)
+        var totalPoints = visit.Attractions.Sum(a => a.Type switch
         {
-            totalPoints += attraction.Type switch
-            {
-                AttractionType.RollerCoaster => 50,
-                AttractionType.Show => 30,
-                AttractionType.Simulator => 20,
-                _ => 10
-            };
-        }
+            AttractionType.RollerCoaster => 50,
+            AttractionType.Show => 30,
+            AttractionType.Simulator => 20,
+            _ => 10
+        });
 
         return totalPoints;
     }
