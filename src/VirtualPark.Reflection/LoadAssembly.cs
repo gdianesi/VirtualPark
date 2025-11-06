9cefca45ec0cd1a4fee0d6113c65355faa73323c
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-11-05 22:14:03 -0300

    RED: GetImplementations_WhenDirectoryExistsButHasNoDll_ShouldReturnEmpty

--- src/VirtualPark.Reflection/LoadAssembly.cs
+++ src/VirtualPark.Reflection/LoadAssembly.cs
@@ -1,4 +1,5 @@
 using System.Reflection;
+using VirtualPark.ReflectionAbstractions;
 
 namespace VirtualPark.Reflection;
 
@@ -8,31 +9,5 @@ public sealed class LoadAssembly<TInterface>(string path) : ILoadAssembly<TInter
     private readonly DirectoryInfo _directory = new(path);
     private List<Type> _implementations = [];
 
-    public List<string?> GetImplementations()
-    {
-        var files = _directory.GetFiles("*.dll").ToList();
-
-        files.ForEach(file =>
-        {
-            Assembly assemblyLoaded = Assembly.LoadFile(file.FullName);
-            var loadedTypes = assemblyLoaded
-                .GetTypes()
-                .Where(t => t.IsClass && typeof(TInterface).IsAssignableFrom(t))
-                .ToList();
-
-            if(loadedTypes.Count == 0)
-            {
-                throw new InvalidOperationException($"No strategies found in assembly '{file.Name}'.");
-            }
-
-            _implementations = _implementations.Union(loadedTypes).ToList();
-        });
-
-        return _implementations.ConvertAll(t => t.FullName);
-    }
-
-    public TInterface GetImplementation(string assemblyName, params object[] args)
-    {
-        throw new NotImplementedException();
-    }
+    
 }
--- tests/VirtualPark.Reflection.Test/LoadAssemblyTest.cs
+++ tests/VirtualPark.Reflection.Test/LoadAssemblyTest.cs
@@ -8,4 +8,21 @@ namespace VirtualPark.Reflection.Test;
 [TestCategory("Reflection")]
 [TestCategory("LoadAssembly")]
 public sealed class LoadAssemblyTest
-{}
+{
+    #region GetImplementations
+
+    [TestMethod]
+    public void GetImplementations_WhenDirectoryExistsButHasNoDll_ShouldReturnEmpty()
+    {
+        var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "PluingsTestDllTest");
+        Directory.CreateDirectory(path);
+
+        var loader = new LoadAssembly<IStrategy>(path);
+
+        var result = loader.GetImplementations();
+
+        result.Should().BeEmpty();
+    }
+
+    #endregion
+}
