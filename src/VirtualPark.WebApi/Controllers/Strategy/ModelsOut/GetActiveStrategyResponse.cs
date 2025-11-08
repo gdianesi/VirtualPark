ddfee4db34459fd4971c42c8610d6561c4b79180
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-11-08 11:56:21 -0300

    REFACTOR: GetActiveStrategyResponse

--- src/VirtualPark.WebApi/Controllers/Strategy/ModelsOut/GetActiveStrategyResponse.cs
+++ src/VirtualPark.WebApi/Controllers/Strategy/ModelsOut/GetActiveStrategyResponse.cs
@@ -3,5 +3,5 @@ namespace VirtualPark.WebApi.Controllers.Strategy.ModelsOut;
 public class GetActiveStrategyResponse(string key, string date)
 {
     public string Key { get; } = key;
-    public string Date { get; } = date;
+    public string? Date { get; } = date;
 }
