3c5b81fb8243a883af7b2bad6ff311089f43de71
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-11-08 17:55:23 -0300

    GREEN : Constructor_ShouldAssignKey_WhenValid

--- src/VirtualPark.BusinessLogic/Strategy/Models/StrategyArgs.cs
+++ src/VirtualPark.BusinessLogic/Strategy/Models/StrategyArgs.cs
@@ -2,6 +2,7 @@ using VirtualPark.BusinessLogic.Validations.Services;
 
 namespace VirtualPark.BusinessLogic.Strategy.Models;
 
-public class StrategyArgs()
+public class StrategyArgs(string strategyKey)
 {
+    public string Key { get; set; } = ValidationServices.ValidateNullOrEmpty(strategyKey);
 }
