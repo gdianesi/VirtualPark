5fc66b5935f43d2e4866fedeeae2a49ff1f8553f
Alfonso Ricaldoni <AR287195@fi365.ort.edu.uy>
2025-11-16 11:52:21 -0300

    RED: validateEntryNFCRequest visitorid property

---
+++ tests/VirtualPark.WebApi.Test/Controllers/Attractions/ModelsIn/ValidateEntryByNfcRequestTest.cs
@@ -0,0 +1,18 @@
+namespace VirtualPark.WebApi.Test.Controllers.Attractions.ModelsIn;
+
+[TestClass]
+[TestCategory("ValidateEntryByNfcRequest")]
+public class ValidateEntryByNfcRequestTest
+{
+    #region VisitorId
+    [TestMethod]
+    public void ValidateEntryByNfcRequest_VisitorIdProperty_GetAndSet_ShouldWorkCorrectly()
+    {
+        var visitorId = "aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee";
+
+        var request = new ValidateEntryByNfcRequest { VisitorId = visitorId };
+
+        request.VisitorId.Should().Be(visitorId);
+    }
+    #endregion
+}
