69fed961458f1aa6e9782e2f7b63e6066b200de3
Alfonso Ricaldoni <AR287195@fi365.ort.edu.uy>
2025-11-16 17:11:25 -0300

    add getVisitorsInAttraction on IvisitRegistrationservice

--- src/VirtualPark.BusinessLogic/VisitRegistrations/Service/IVisitRegistrationService.cs
+++ src/VirtualPark.BusinessLogic/VisitRegistrations/Service/IVisitRegistrationService.cs
@@ -1,4 +1,5 @@
 using VirtualPark.BusinessLogic.Attractions.Entity;
+using VirtualPark.BusinessLogic.VisitorsProfile.Entity;
 using VirtualPark.BusinessLogic.VisitsScore.Models;
 
 namespace VirtualPark.BusinessLogic.VisitRegistrations.Service;
@@ -9,4 +10,5 @@ public interface IVisitRegistrationService
     public void UpToAttraction(Guid visitId, Guid attractionId);
     public void DownToAttraction(Guid visitId);
     public List<Attraction> GetAttractionsForTicket(Guid visitorId);
+    public List<VisitorProfile> GetVisitorsInAttraction(Guid attractionId);
 }
