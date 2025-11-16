8d349914c70edf2db2231f1e5ef6459b9ca8e255
Alfonso Ricaldoni <AR287195@fi365.ort.edu.uy>
2025-11-16 17:55:46 -0300

    RED: visitorInAttractionResponse id

---
+++ tests/VirtualPark.WebApi.Test/Controllers/Users/ModelsOut/VisitorInAttractionResponseTest.cs
@@ -0,0 +1,22 @@
+namespace VirtualPark.WebApi.Test.Controllers.Users.ModelsOut;
+
+[TestClass]
+[TestCategory("ModelsOut")]
+[TestCategory("VisitorInAttractionResponse")]
+public class VisitorInAttractionResponseTest
+{
+    #region VisitorProfileId
+    [TestMethod]
+    [TestCategory("Validation")]
+    public void VisitorProfileId_Getter_ReturnsAssignedValue()
+    {
+        var visitorProfileId = Guid.NewGuid();
+        var response = new VisitorInAttractionResponse
+        {
+            VisitorProfileId = visitorProfileId
+        };
+
+        response.VisitorProfileId.Should().Be(visitorProfileId);
+    }
+    #endregion
+}
